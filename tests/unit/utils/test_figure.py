9db428c362502db2175030838fd0d2916f24e0bc
Thibaut Durand <durand.tibo@gmail.com>
2024-08-17 12:13:55 -0700

    Add `figure2html` (#35)

---
+++ tests/unit/utils/test_figure.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import pytest
+from matplotlib import pyplot as plt
+
+from arkas.utils.figure import MISSING_FIGURE_MESSAGE, figure2html
+
+#################################
+#     Tests for figure2html     #
+#################################
+
+
+def test_figure2html() -> None:
+    fig, _ = plt.subplots()
+    assert isinstance(figure2html(fig), str)
+
+
+@pytest.mark.parametrize("close_fig", [True, False])
+def test_figure2html_close_fig(close_fig: bool) -> None:
+    fig, _ = plt.subplots()
+    assert isinstance(figure2html(fig, close_fig=close_fig), str)
+
+
+@pytest.mark.parametrize("reactive", [True, False])
+def test_figure2html_reactive(reactive: bool) -> None:
+    fig, _ = plt.subplots()
+    assert isinstance(figure2html(fig, reactive=reactive), str)
+
+
+def test_figure2html_none() -> None:
+    assert figure2html(None) == MISSING_FIGURE_MESSAGE
