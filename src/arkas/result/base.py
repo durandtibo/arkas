9c4d386b5c9953b699355a50834cd120e7eba2d7
Thibaut Durand <durand.tibo@gmail.com>
2024-08-17 13:57:12 -0700

    Add `BaseSection` and update `BaseResult`

--- src/arkas/result/base.py
+++ src/arkas/result/base.py
@@ -1,14 +1,10 @@
-r"""Contain the base class to implement a result."""
+r"""Contain the base class to implement a section."""
 
 from __future__ import annotations
 
 __all__ = ["BaseResult"]
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from collections.abc import Sequence
 
 
 class BaseResult(ABC):
@@ -28,35 +24,4 @@ class BaseResult(ABC):
 
         Returns:
             The plots.
-        """
-
-    @abstractmethod
-    def render_html_body(self, number: str = "", tags: Sequence[str] = (), depth: int = 0) -> str:
-        r"""Return the HTML body associated to the result.
-
-        Args:
-            number: The result number.
-            tags: The tags associated to the result.
-            depth: The depth in the report.
-
-        Returns:
-            The HTML body associated to the result.
-        """
-
-    @abstractmethod
-    def render_html_toc(
-        self, number: str = "", tags: Sequence[str] = (), depth: int = 0, max_depth: int = 1
-    ) -> str:
-        r"""Return the HTML table of content (TOC) associated to the
-        result.
-
-        Args:
-            number: The result number associated to the
-                result.
-            tags: The tags associated to the result.
-            depth: The depth in the report.
-            max_depth: The maximum depth to generate in the TOC.
-
-        Returns:
-            The HTML table of content associated to the result.
         """
