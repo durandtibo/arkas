70270e4891a1dc9d983fcc8d0890ec748d433c48
Thibaut Durand <durand.tibo@gmail.com>
2024-08-18 14:08:29 -0700

    Add `AccuracyEvaluator`

--- tests/unit/evaluator/test_accuracy.py
+++ tests/unit/evaluator/test_accuracy.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import numpy as np
 
 from arkas.evaluator import AccuracyEvaluator
-from arkas.result import AccuracyResult
-
+from arkas.result import AccuracyResult, Result
 
 #######################################
 #     Tests for AccuracyEvaluator     #
@@ -24,4 +23,14 @@ def test_accuracy_evaluator_evaluate() -> None:
         AccuracyEvaluator(y_true="target", y_pred="pred")
         .evaluate({"pred": np.array([3, 2, 0, 1, 0]), "target": np.array([1, 2, 3, 2, 1])})
         .equal(AccuracyResult(y_true=np.array([1, 2, 3, 2, 1]), y_pred=np.array([3, 2, 0, 1, 0])))
+    )
+
+
+def test_accuracy_evaluator_evaluate_lazy_false() -> None:
+    assert (
+        AccuracyEvaluator(y_true="target", y_pred="pred")
+        .evaluate(
+            {"pred": np.array([3, 2, 0, 1, 0]), "target": np.array([3, 2, 0, 1, 0])}, lazy=False
+        )
+        .equal(Result(metrics={"accuracy": 1.0, "count": 5}))
     )
