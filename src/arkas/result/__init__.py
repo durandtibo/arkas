12a73aef497b6e5db3cff6c74bc2d629cc6a6214
Thibaut Durand <durand.tibo@gmail.com>
2024-08-18 12:45:42 -0700

    Add `AccuracyResult`

--- src/arkas/result/__init__.py
+++ src/arkas/result/__init__.py
@@ -2,8 +2,9 @@ r"""Contain results."""
 
 from __future__ import annotations
 
-__all__ = ["BaseResult", "BinaryClassificationResult", "Result"]
+__all__ = ["AccuracyResult", "BaseResult", "BinaryClassificationResult", "Result"]
 
+from arkas.result.accuracy import AccuracyResult
 from arkas.result.base import BaseResult
 from arkas.result.binary_classification import BinaryClassificationResult
 from arkas.result.vanilla import Result
