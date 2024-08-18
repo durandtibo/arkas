c819e58ddd49f5d16a66822b17cc26fc591ccc18
Thibaut Durand <durand.tibo@gmail.com>
2024-08-18 08:55:32 -0700

    Add `ContentSection`

--- src/arkas/section/__init__.py
+++ src/arkas/section/__init__.py
@@ -1,1 +1,8 @@
 r"""Contain sections."""
+
+from __future__ import annotations
+
+__all__ = ["BaseSection", "ContentSection"]
+
+from arkas.section.base import BaseSection
+from arkas.section.content import ContentSection
