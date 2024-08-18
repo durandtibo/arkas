8783a00e52f35a0636dfdeacdc9c4b581e5ffffa
Thibaut Durand <durand.tibo@gmail.com>
2024-08-18 00:08:59 -0700

    Add `DataFrameTransformer` (#50)

--- src/arkas/data/transformer/__init__.py
+++ src/arkas/data/transformer/__init__.py
@@ -2,10 +2,11 @@ r"""Contain the data transformers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseTransformer", "is_transformer_config", "setup_transformer"]
+__all__ = ["BaseTransformer", "DataFrameTransformer", "is_transformer_config", "setup_transformer"]
 
 from arkas.data.transformer.base import (
     BaseTransformer,
     is_transformer_config,
     setup_transformer,
 )
+from arkas.data.transformer.dataframe import DataFrameTransformer
