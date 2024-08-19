be482fd3b7f5fc631981dc9a9948b0a0854f44f6
Thibaut Durand <durand.tibo@gmail.com>
2024-08-18 18:18:25 -0700

    Add `ColumnToArrayTransformer` (#66)

--- src/arkas/data/transformer/__init__.py
+++ src/arkas/data/transformer/__init__.py
@@ -2,11 +2,18 @@ r"""Contain the data transformers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseTransformer", "DataFrameTransformer", "is_transformer_config", "setup_transformer"]
+__all__ = [
+    "BaseTransformer",
+    "ColumnToArrayTransformer",
+    "DataFrameTransformer",
+    "is_transformer_config",
+    "setup_transformer",
+]
 
 from arkas.data.transformer.base import (
     BaseTransformer,
     is_transformer_config,
     setup_transformer,
 )
+from arkas.data.transformer.column import ColumnToArrayTransformer
 from arkas.data.transformer.dataframe import DataFrameTransformer
