63be6a8f0483d7a3085c6002dda1e00ee1d6c361
Thibaut Durand <durand.tibo@gmail.com>
2024-08-19 18:39:26 -0700

    Add `AveragePrecisionResult`

--- src/arkas/result/__init__.py
+++ src/arkas/result/__init__.py
@@ -2,9 +2,17 @@ r"""Contain results."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracyResult", "BaseResult", "BinaryClassificationResult", "Result", "EmptyResult"]
+__all__ = [
+    "AccuracyResult",
+    "AveragePrecisionResult",
+    "BaseResult",
+    "BinaryClassificationResult",
+    "Result",
+    "EmptyResult",
+]
 
 from arkas.result.accuracy import AccuracyResult
+from arkas.result.ap import AveragePrecisionResult
 from arkas.result.base import BaseResult
 from arkas.result.binary_classification import BinaryClassificationResult
 from arkas.result.vanilla import EmptyResult, Result
