0adb284a663729fb0c33cbdbb9024bdf15682717
Thibaut Durand <durand.tibo@gmail.com>
2024-08-20 08:54:47 -0700

    Fix formatting

--- tests/unit/result/test_accuracy.py
+++ tests/unit/result/test_accuracy.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
-from coola import objects_are_equal, objects_are_allclose
+from coola import objects_are_allclose, objects_are_equal
 
 from arkas.result import AccuracyResult
 
