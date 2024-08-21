a5a64731cc4e02bc4e248532d5cc93079fe6d6e1
Thibaut Durand <durand.tibo@gmail.com>
2024-08-21 15:08:13 -0700

    Add `MergedResult`

--- src/arkas/result/__init__.py
+++ src/arkas/result/__init__.py
@@ -10,10 +10,12 @@ __all__ = [
     "Result",
     "EmptyResult",
     "BalancedAccuracyResult",
+    "MergedResult",
 ]
 
 from arkas.result.accuracy import AccuracyResult, BalancedAccuracyResult
 from arkas.result.ap import AveragePrecisionResult
 from arkas.result.base import BaseResult
 from arkas.result.binary_classification import BinaryClassificationResult
+from arkas.result.merge import MergedResult
 from arkas.result.vanilla import EmptyResult, Result
