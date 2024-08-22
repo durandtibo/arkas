4524ac0edbc2adcd177a818318ae384eb1813ca4
Thibaut Durand <durand.tibo@gmail.com>
2024-08-22 07:02:51 -0700

    Add `PrecisionResult` (#92)

--- src/arkas/result/__init__.py
+++ src/arkas/result/__init__.py
@@ -5,12 +5,13 @@ from __future__ import annotations
 __all__ = [
     "AccuracyResult",
     "AveragePrecisionResult",
+    "BalancedAccuracyResult",
     "BaseResult",
     "BinaryClassificationResult",
-    "Result",
     "EmptyResult",
-    "BalancedAccuracyResult",
     "MergedResult",
+    "PrecisionResult",
+    "Result",
 ]
 
 from arkas.result.accuracy import AccuracyResult, BalancedAccuracyResult
@@ -18,4 +19,5 @@ from arkas.result.ap import AveragePrecisionResult
 from arkas.result.base import BaseResult
 from arkas.result.binary_classification import BinaryClassificationResult
 from arkas.result.merge import MergedResult
+from arkas.result.precision import PrecisionResult
 from arkas.result.vanilla import EmptyResult, Result
