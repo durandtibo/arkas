2b0dadd7b46f4dd62745c1f2801a38f8f62ff9a2
Thibaut Durand <durand.tibo@gmail.com>
2024-08-25 13:52:16 -0700

    Improve `roc_auc_metrics`

--- src/arkas/metric/__init__.py
+++ src/arkas/metric/__init__.py
@@ -6,26 +6,29 @@ __all__ = [
     "accuracy_metrics",
     "average_precision_metrics",
     "balanced_accuracy_metrics",
+    "binary_average_precision_metrics",
     "binary_confusion_matrix_metrics",
+    "binary_jaccard_metrics",
     "binary_precision_metrics",
     "binary_recall_metrics",
+    "binary_roc_auc_metrics",
     "confusion_matrix_metrics",
     "fbeta_metrics",
     "jaccard_metrics",
+    "multiclass_average_precision_metrics",
     "multiclass_confusion_matrix_metrics",
+    "multiclass_jaccard_metrics",
     "multiclass_precision_metrics",
     "multiclass_recall_metrics",
+    "multiclass_roc_auc_metrics",
+    "multilabel_average_precision_metrics",
+    "multilabel_jaccard_metrics",
     "multilabel_precision_metrics",
     "multilabel_recall_metrics",
+    "multilabel_roc_auc_metrics",
     "precision_metrics",
     "recall_metrics",
     "roc_auc_metrics",
-    "binary_jaccard_metrics",
-    "multiclass_jaccard_metrics",
-    "multilabel_jaccard_metrics",
-    "binary_average_precision_metrics",
-    "multiclass_average_precision_metrics",
-    "multilabel_average_precision_metrics",
 ]
 
 from arkas.metric.accuracy import accuracy_metrics, balanced_accuracy_metrics
@@ -59,4 +62,9 @@ from arkas.metric.recall import (
     multilabel_recall_metrics,
     recall_metrics,
 )
-from arkas.metric.roc_auc import roc_auc_metrics
+from arkas.metric.roc_auc import (
+    binary_roc_auc_metrics,
+    multiclass_roc_auc_metrics,
+    multilabel_roc_auc_metrics,
+    roc_auc_metrics,
+)
