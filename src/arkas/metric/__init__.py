87d7b4adec3154e35f506018ec6c332017dbd62c
Thibaut Durand <durand.tibo@gmail.com>
2024-08-24 18:18:04 -0700

    Move `average_precision_metrics` to `metric`

--- src/arkas/metric/__init__.py
+++ src/arkas/metric/__init__.py
@@ -2,7 +2,13 @@ r"""Contain functions to compute metrics."""
 
 from __future__ import annotations
 
-__all__ = ["accuracy_metrics", "balanced_accuracy_metrics", "precision_metrics"]
+__all__ = [
+    "average_precision_metrics",
+    "accuracy_metrics",
+    "balanced_accuracy_metrics",
+    "precision_metrics",
+]
 
 from arkas.metric.accuracy import accuracy_metrics, balanced_accuracy_metrics
+from arkas.metric.ap import average_precision_metrics
 from arkas.metric.precision import precision_metrics
