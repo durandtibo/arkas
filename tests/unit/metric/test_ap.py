bcc52028b4834ff253302a9553ca529d81c0ccec
Thibaut Durand <durand.tibo@gmail.com>
2024-08-24 21:15:23 -0700

    Improve `average_precision_metrics` (#127)

--- tests/unit/metric/test_ap.py
+++ tests/unit/metric/test_ap.py
@@ -88,6 +88,39 @@ def test_average_precision_metrics_binary_incorrect_shape() -> None:
         )
 
 
+def test_average_precision_metrics_binary_nans() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([1, 0, 0, 1, 1, float("nan")]),
+            y_score=np.array([2, -1, 0, 3, 1, float("nan")]),
+            label_type="binary",
+        ),
+        {"count": 5, "average_precision": 1.0},
+    )
+
+
+def test_average_precision_metrics_binary_y_true_nan() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([1, 0, 0, 1, 1, float("nan")]),
+            y_score=np.array([2, -1, 0, 3, 1, 0]),
+            label_type="binary",
+        ),
+        {"count": 5, "average_precision": 1.0},
+    )
+
+
+def test_average_precision_metrics_binary_y_score_nan() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([1, 0, 0, 1, 1, 0]),
+            y_score=np.array([2, -1, 0, float("nan"), 1, -3]),
+            label_type="binary",
+        ),
+        {"count": 5, "average_precision": 1.0},
+    )
+
+
 def test_average_precision_metrics_multiclass_correct() -> None:
     assert objects_are_equal(
         average_precision_metrics(
@@ -200,6 +233,87 @@ def test_average_precision_metrics_multiclass_prefix_suffix() -> None:
             "prefix_macro_average_precision_suffix": 1.0,
             "prefix_micro_average_precision_suffix": 1.0,
             "prefix_weighted_average_precision_suffix": 1.0,
+        },
+    )
+
+
+def test_average_precision_metrics_multiclass_nans() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([0, 0, 1, 1, 2, 2, float("nan")]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, float("nan")],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [float("nan"), float("nan"), float("nan")],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+        ),
+        {
+            "average_precision": np.array([1.0, 1.0, 1.0]),
+            "count": 4,
+            "macro_average_precision": 1.0,
+            "micro_average_precision": 1.0,
+            "weighted_average_precision": 1.0,
+        },
+    )
+
+
+def test_average_precision_metrics_multiclass_y_true_nan() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([0, 0, 1, 1, 2, 2, float("nan")]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, 0.2],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [0.1, 0.2, 0.7],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+        ),
+        {
+            "average_precision": np.array([1.0, 1.0, 1.0]),
+            "count": 6,
+            "macro_average_precision": 1.0,
+            "micro_average_precision": 1.0,
+            "weighted_average_precision": 1.0,
+        },
+    )
+
+
+def test_average_precision_metrics_multiclass_y_score_nan() -> None:
+    assert objects_are_equal(
+        average_precision_metrics(
+            y_true=np.array([0, 0, 1, 1, 2, 2, 0]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, float("nan")],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [float("nan"), float("nan"), float("nan")],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+        ),
+        {
+            "average_precision": np.array([1.0, 1.0, 1.0]),
+            "count": 5,
+            "macro_average_precision": 1.0,
+            "micro_average_precision": 1.0,
+            "weighted_average_precision": 1.0,
         },
     )
 
