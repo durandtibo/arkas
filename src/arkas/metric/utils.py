3c6732a81411f5b7d5a6c2ad23f9d8e24ca1324b
Thibaut Durand <durand.tibo@gmail.com>
2024-08-24 20:34:55 -0700

    Add `preprocess_true_pred` (#125)

--- src/arkas/metric/utils.py
+++ src/arkas/metric/utils.py
@@ -2,7 +2,7 @@ r"""Contain utility functions to compute metrics."""
 
 from __future__ import annotations
 
-__all__ = ["multi_isnan"]
+__all__ = ["multi_isnan", "preprocess_true_pred", "check_nan_true_pred"]
 
 
 from typing import TYPE_CHECKING
@@ -46,3 +46,68 @@ def multi_isnan(arrays: Sequence[np.ndarray]) -> np.ndarray:
     for arr in arrays[1:]:
         mask = np.logical_or(mask, np.isnan(arr))
     return mask
+
+
+def preprocess_true_pred(
+    y_true: np.ndarray, y_pred: np.ndarray, nan: str = "keep"
+) -> tuple[np.ndarray, np.ndarray]:
+    r"""Preprocess ``y_true`` and ``y_pred`` arrays.
+
+    Args:
+        y_true: The ground truth target labels.
+        y_pred: The predicted labels.
+        nan: Indicate how to process the nan values.
+            If ``'keep'``, the nan values are kept.
+            If ``'remove'``, the nan values are removed.
+
+    Returns:
+        A tuple with the preprocessed ``y_true`` and ``y_pred``
+            arrays.
+
+    Raises:
+        RuntimeError: if an invalid value is passed to ``nan``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from arkas.metric.utils import preprocess_true_pred
+    >>> y_true = np.array([1, 0, 0, 1, 1, float("nan")])
+    >>> y_pred = np.array([0, 1, 0, 1, float("nan"), 1])
+    >>> preprocess_true_pred(y_true, y_pred)
+    (array([ 1.,  0.,  0.,  1.,  1., nan]), array([ 0.,  1.,  0.,  1., nan,  1.]))
+    >>> preprocess_true_pred(y_true, y_pred, nan="remove")
+    (array([1., 0., 0., 1.]), array([0., 1., 0., 1.]))
+
+    ```
+    """
+    check_nan_true_pred(nan)
+    if nan == "keep":
+        return y_true, y_pred
+    mask = np.logical_not(multi_isnan([y_true, y_pred]))
+    return y_true[mask], y_pred[mask]
+
+
+def check_nan_true_pred(nan: str) -> None:
+    r"""Check if the value is valid or not.
+
+    Args:
+        nan: Indicate how to process the nan values.
+            The valid values are ``'keep'`` and ``'remove'``.
+
+    Raises:
+        RuntimeError: if an invalid value is passed to ``nan``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arkas.metric.utils import check_nan_true_pred
+    >>> check_nan_true_pred(nan="remove")
+
+    ```
+    """
+    if nan not in {"keep", "remove"}:
+        msg = f"Incorrect 'nan': {nan}. The valid values are 'keep' and 'remove'"
+        raise RuntimeError(msg)
--- tests/unit/metric/test_utils.py
+++ tests/unit/metric/test_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from coola import objects_are_equal
 
-from arkas.metric.utils import multi_isnan
+from arkas.metric.utils import check_nan_true_pred, multi_isnan, preprocess_true_pred
 
 #################################
 #     Tests for multi_isnan     #
@@ -46,3 +46,62 @@ def test_multi_isnan_3_arrays() -> None:
 def test_multi_isnan_empty() -> None:
     with pytest.raises(RuntimeError, match="'arrays' cannot be empty"):
         multi_isnan([])
+
+
+##########################################
+#     Tests for preprocess_true_pred     #
+##########################################
+
+
+def test_preprocess_true_pred_no_nan() -> None:
+    assert objects_are_equal(
+        preprocess_true_pred(y_true=np.array([1, 0, 0, 1, 1]), y_pred=np.array([0, 1, 0, 1, 1])),
+        (np.array([1, 0, 0, 1, 1]), np.array([0, 1, 0, 1, 1])),
+    )
+
+
+def test_preprocess_true_pred_keep_nan() -> None:
+    assert objects_are_equal(
+        preprocess_true_pred(
+            y_true=np.array([1, 0, 0, 1, 1, float("nan")]),
+            y_pred=np.array([0, 1, 0, 1, float("nan"), 1]),
+        ),
+        (
+            np.array([1, 0, 0, 1, 1, float("nan")]),
+            np.array([0, 1, 0, 1, float("nan"), 1]),
+        ),
+        equal_nan=True,
+    )
+
+
+def test_preprocess_true_pred_remove_nan() -> None:
+    assert objects_are_equal(
+        preprocess_true_pred(
+            y_true=np.array([1, 0, 0, 1, 1, float("nan")]),
+            y_pred=np.array([0, 1, 0, 1, float("nan"), 1]),
+            nan="remove",
+        ),
+        (np.array([1.0, 0.0, 0.0, 1.0]), np.array([0.0, 1.0, 0.0, 1.0])),
+    )
+
+
+def test_preprocess_true_pred_nan_incorrect() -> None:
+    with pytest.raises(RuntimeError, match="Incorrect 'nan': incorrect"):
+        preprocess_true_pred(
+            y_true=np.array([1, 0, 0, 1, 1]), y_pred=np.array([0, 1, 0, 1, 1]), nan="incorrect"
+        )
+
+
+#########################################
+#     Tests for check_nan_true_pred     #
+#########################################
+
+
+def test_check_nan_true_pred_valid() -> None:
+    check_nan_true_pred("keep")
+    check_nan_true_pred("remove")
+
+
+def test_check_nan_true_pred_incorrect() -> None:
+    with pytest.raises(RuntimeError, match="Incorrect 'nan': incorrect"):
+        check_nan_true_pred(nan="incorrect")
