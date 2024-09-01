33537debe029efe70cbd9cc68f0fbbe955735b84
Thibaut Durand <durand.tibo@gmail.com>
2024-08-31 22:49:45 -0700

    Add `MultilabelAveragePrecisionEvaluator`

---
+++ tests/unit/evaluator/test_multilabel_ap.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+import numpy as np
+import polars as pl
+from coola import objects_are_equal
+
+from arkas.evaluator import MultilabelAveragePrecisionEvaluator
+from arkas.result import EmptyResult, MultilabelAveragePrecisionResult, Result
+
+#########################################################
+#     Tests for MultilabelAveragePrecisionEvaluator     #
+#########################################################
+
+
+def test_multilabel_average_precision_evaluator_repr() -> None:
+    assert repr(MultilabelAveragePrecisionEvaluator(y_true="target", y_score="pred")).startswith(
+        "MultilabelAveragePrecisionEvaluator("
+    )
+
+
+def test_multilabel_average_precision_evaluator_str() -> None:
+    assert str(MultilabelAveragePrecisionEvaluator(y_true="target", y_score="pred")).startswith(
+        "MultilabelAveragePrecisionEvaluator("
+    )
+
+
+def test_multilabel_average_precision_evaluator_evaluate() -> None:
+    assert (
+        MultilabelAveragePrecisionEvaluator(y_true="target", y_score="pred")
+        .evaluate(
+            {
+                "pred": np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+                "target": np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+            }
+        )
+        .equal(
+            MultilabelAveragePrecisionResult(
+                y_true=np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+                y_score=np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+            )
+        )
+    )
+
+
+def test_multilabel_average_precision_evaluator_evaluate_lazy_false() -> None:
+    result = MultilabelAveragePrecisionEvaluator(y_true="target", y_score="pred").evaluate(
+        {
+            "pred": np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+            "target": np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+        },
+        lazy=False,
+    )
+    assert isinstance(result, Result)
+    assert objects_are_equal(
+        result.compute_metrics(),
+        {
+            "average_precision": np.array([1.0, 1.0, 1.0]),
+            "count": 5,
+            "macro_average_precision": 1.0,
+            "micro_average_precision": 1.0,
+            "weighted_average_precision": 1.0,
+        },
+    )
+    assert objects_are_equal(result.generate_figures(), {})
+
+
+def test_multilabel_average_precision_evaluator_evaluate_missing_keys() -> None:
+    assert (
+        MultilabelAveragePrecisionEvaluator(y_true="target", y_score="prediction")
+        .evaluate(
+            {
+                "pred": np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+                "target": np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+            }
+        )
+        .equal(EmptyResult())
+    )
+
+
+def test_multilabel_average_precision_evaluator_evaluate_lazy_false_missing_keys() -> None:
+    assert (
+        MultilabelAveragePrecisionEvaluator(y_true="target", y_score="missing")
+        .evaluate(
+            {
+                "pred": np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+                "target": np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+            },
+            lazy=False,
+        )
+        .equal(EmptyResult())
+    )
+
+
+def test_multilabel_average_precision_evaluator_evaluate_dataframe() -> None:
+    assert (
+        MultilabelAveragePrecisionEvaluator(y_true="target", y_score="pred")
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+                    "target": np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+                }
+            )
+        )
+        .equal(
+            MultilabelAveragePrecisionResult(
+                y_true=np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, 1]]),
+                y_score=np.array([[2, -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]),
+            )
+        )
+    )
