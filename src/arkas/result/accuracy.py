77c3a475c078481a9e1a93806370eec9cba82d73
Thibaut Durand <durand.tibo@gmail.com>
2024-09-07 09:58:49 -0700

    Improve accuracy results (#269)

--- src/arkas/result/accuracy.py
+++ src/arkas/result/accuracy.py
@@ -9,6 +9,7 @@ from typing import TYPE_CHECKING, Any
 from coola import objects_are_equal
 
 from arkas.metric.accuracy import accuracy_metrics, balanced_accuracy_metrics
+from arkas.metric.utils import check_same_shape_pred
 from arkas.result.base import BaseResult
 
 if TYPE_CHECKING:
@@ -47,7 +48,7 @@ class AccuracyResult(BaseResult):
         self._y_true = y_true.ravel()
         self._y_pred = y_pred.ravel()
 
-        self._check_inputs()
+        check_same_shape_pred(y_true=self._y_true, y_pred=self._y_pred)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}(y_true={self._y_true.shape}, y_pred={self._y_pred.shape})"
@@ -76,14 +77,6 @@ class AccuracyResult(BaseResult):
         self, prefix: str = "", suffix: str = ""  # noqa: ARG002
     ) -> dict[str, float]:
         return {}
-
-    def _check_inputs(self) -> None:
-        if self._y_true.shape != self._y_pred.shape:
-            msg = (
-                f"'y_true' and 'y_pred' have different shapes: {self._y_true.shape} vs "
-                f"{self._y_pred.shape}"
-            )
-            raise ValueError(msg)
 
 
 class BalancedAccuracyResult(BaseResult):
@@ -118,7 +111,7 @@ class BalancedAccuracyResult(BaseResult):
         self._y_true = y_true.ravel()
         self._y_pred = y_pred.ravel()
 
-        self._check_inputs()
+        check_same_shape_pred(y_true=self._y_true, y_pred=self._y_pred)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}(y_true={self._y_true.shape}, y_pred={self._y_pred.shape})"
@@ -147,11 +140,3 @@ class BalancedAccuracyResult(BaseResult):
         self, prefix: str = "", suffix: str = ""  # noqa: ARG002
     ) -> dict[str, float]:
         return {}
-
-    def _check_inputs(self) -> None:
-        if self._y_true.shape != self._y_pred.shape:
-            msg = (
-                f"'y_true' and 'y_pred' have different shapes: {self._y_true.shape} vs "
-                f"{self._y_pred.shape}"
-            )
-            raise ValueError(msg)
