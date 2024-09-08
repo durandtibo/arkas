2b868db4fb6d2b5cc70f47a25aa5c8a10b740047
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 15:29:46 -0700

    Improve `MultilabelFbetaScoreEvaluator`

--- src/arkas/evaluator/multilabel_fbeta.py
+++ src/arkas/evaluator/multilabel_fbeta.py
@@ -7,22 +7,20 @@ __all__ = ["MultilabelFbetaScoreEvaluator"]
 import logging
 from typing import TYPE_CHECKING
 
-from arkas.evaluator.base import BaseLazyEvaluator
-from arkas.result import EmptyResult, MultilabelFbetaScoreResult
+from arkas.evaluator.lazy import BaseLazyEvaluator
+from arkas.result import MultilabelFbetaScoreResult, Result
 from arkas.utils.array import to_array
-from arkas.utils.data import find_keys, find_missing_keys
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
     import polars as pl
 
-    from arkas.result import BaseResult
 
 logger = logging.getLogger(__name__)
 
 
-class MultilabelFbetaScoreEvaluator(BaseLazyEvaluator):
+class MultilabelFbetaScoreEvaluator(BaseLazyEvaluator[MultilabelFbetaScoreResult]):
     r"""Implement the F-beta evaluator for multilabel labels.
 
     Args:
@@ -30,6 +28,8 @@ class MultilabelFbetaScoreEvaluator(BaseLazyEvaluator):
             labels.
         y_pred: The key or column name of the predicted labels.
         betas: The betas used to compute the F-beta scores.
+        drop_nulls: If ``True``, the rows with null values in
+            ``y_true`` or ``y_pred`` columns are dropped.
 
     Example usage:
 
@@ -39,7 +39,7 @@ class MultilabelFbetaScoreEvaluator(BaseLazyEvaluator):
     >>> from arkas.evaluator import MultilabelFbetaScoreEvaluator
     >>> evaluator = MultilabelFbetaScoreEvaluator(y_true="target", y_pred="pred")
     >>> evaluator
-    MultilabelFbetaScoreEvaluator(y_true=target, y_pred=pred, betas=(1,))
+    MultilabelFbetaScoreEvaluator(y_true=target, y_pred=pred, betas=(1,), drop_nulls=True)
     >>> data = pl.DataFrame(
     ...     {
     ...         "pred": [[1, 0, 0], [0, 1, 1], [0, 1, 1], [1, 0, 0], [1, 0, 0]],
@@ -54,32 +54,35 @@ class MultilabelFbetaScoreEvaluator(BaseLazyEvaluator):
     ```
     """
 
-    def __init__(self, y_true: str, y_pred: str, betas: Sequence[float] = (1,)) -> None:
+    def __init__(
+        self, y_true: str, y_pred: str, betas: Sequence[float] = (1,), drop_nulls: bool = True
+    ) -> None:
+        super().__init__(drop_nulls=drop_nulls)
         self._y_true = y_true
         self._y_pred = y_pred
         self._betas = tuple(betas)
 
     def __repr__(self) -> str:
         return (
-            f"{self.__class__.__qualname__}(y_true={self._y_true}, "
-            f"y_pred={self._y_pred}, betas={self._betas})"
+            f"{self.__class__.__qualname__}(y_true={self._y_true}, y_pred={self._y_pred}, "
+            f"betas={self._betas}, drop_nulls={self._drop_nulls})"
         )
 
-    def _evaluate(self, data: dict | pl.DataFrame) -> BaseResult:
+    def evaluate(
+        self, data: pl.DataFrame, lazy: bool = True
+    ) -> MultilabelFbetaScoreResult | Result:
         logger.info(
-            f"Evaluating the multilabel F-beta | y_true={self._y_true} | y_pred={self._y_pred} | "
-            f"betas={self._betas}"
+            f"Evaluating the multilabel F-beta score | y_true={self._y_true} | "
+            f"y_pred={self._y_pred} | drop_nulls={self._drop_nulls}"
         )
-        if missing_keys := find_missing_keys(
-            keys=find_keys(data), queries=[self._y_pred, self._y_true]
-        ):
-            logger.warning(
-                "Skipping the multilabel F-beta evaluation because some keys are missing: "
-                f"{sorted(missing_keys)}"
-            )
-            return EmptyResult()
+        return self._evaluate(data, lazy)
+
+    def _compute_result(self, data: pl.DataFrame) -> MultilabelFbetaScoreResult:
         return MultilabelFbetaScoreResult(
             y_true=to_array(data[self._y_true]),
             y_pred=to_array(data[self._y_pred]),
             betas=self._betas,
         )
+
+    def _get_columns(self) -> tuple[str, ...]:
+        return (self._y_true, self._y_pred)
