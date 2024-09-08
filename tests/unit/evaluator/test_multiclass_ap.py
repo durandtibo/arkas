cf1ef00feedcf77ffcf6f2000804eef227b6e944
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 14:43:39 -0700

    Improve `MulticlassAveragePrecisionEvaluator`

--- tests/unit/evaluator/test_multiclass_ap.py
+++ tests/unit/evaluator/test_multiclass_ap.py
@@ -188,16 +188,39 @@ def test_multiclass_average_precision_evaluator_evaluate_drop_nulls_false() -> N
         .evaluate(
             pl.DataFrame(
                 {
-                    "pred": [3, 2, 0, 1, 0, None, 1, None],
-                    "target": [1, 2, 3, 2, 1, 2, None, None],
-                    "col": [1, None, 3, 4, 5, None, 7, None],
-                }
+                    "pred": [
+                        [0.7, 0.2, 0.1],
+                        [0.4, 0.3, 0.3],
+                        [0.1, 0.8, 0.1],
+                        [0.2, 0.5, 0.3],
+                        [0.3, 0.2, 0.5],
+                        [0.1, 0.2, 0.7],
+                        None,
+                        [0.1, 0.2, 0.7],
+                        [None, None, None],
+                    ],
+                    "target": [0, 0, 1, 1, 2, 2, 0, None, None],
+                    "col": [1, None, 3, 4, 5, 6, None, 8, None],
+                },
+                schema={"pred": pl.Array(pl.Float64, 3), "target": pl.Int64, "col": pl.Int64},
             )
         )
         .equal(
             MulticlassAveragePrecisionResult(
-                y_true=np.array([1.0, 2.0, 3.0, 2.0, 1.0, 2.0, float("nan"), float("nan")]),
-                y_score=np.array([3.0, 2.0, 0.0, 1.0, 0.0, float("nan"), 1.0, float("nan")]),
+                y_true=np.array([0, 0, 1, 1, 2, 2, 0, float("nan"), float("nan")]),
+                y_score=np.array(
+                    [
+                        [0.7, 0.2, 0.1],
+                        [0.4, 0.3, 0.3],
+                        [0.1, 0.8, 0.1],
+                        [0.2, 0.5, 0.3],
+                        [0.3, 0.2, 0.5],
+                        [0.1, 0.2, 0.7],
+                        [float("nan"), float("nan"), float("nan")],
+                        [0.1, 0.2, 0.7],
+                        [float("nan"), float("nan"), float("nan")],
+                    ]
+                ),
             ),
             equal_nan=True,
         )
