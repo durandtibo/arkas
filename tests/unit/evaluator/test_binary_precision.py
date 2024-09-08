ad1cce8133dfc8078d2fc7d7c8de64c20e055b97
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 11:46:05 -0700

    Improve `BinaryPrecisionEvaluator` (#313)

--- tests/unit/evaluator/test_binary_precision.py
+++ tests/unit/evaluator/test_binary_precision.py
@@ -28,7 +28,7 @@ def test_binary_precision_evaluator_str() -> None:
 def test_binary_precision_evaluator_evaluate() -> None:
     assert (
         BinaryPrecisionEvaluator(y_true="target", y_pred="pred")
-        .evaluate({"pred": np.array([1, 0, 0, 1, 1]), "target": np.array([1, 0, 1, 0, 1])})
+        .evaluate(pl.DataFrame({"pred": [1, 0, 0, 1, 1], "target": [1, 0, 1, 0, 1]}))
         .equal(
             BinaryPrecisionResult(
                 y_true=np.array([1, 0, 1, 0, 1]), y_pred=np.array([1, 0, 0, 1, 1])
@@ -39,7 +39,7 @@ def test_binary_precision_evaluator_evaluate() -> None:
 
 def test_binary_precision_evaluator_evaluate_lazy_false() -> None:
     result = BinaryPrecisionEvaluator(y_true="target", y_pred="pred").evaluate(
-        {"pred": np.array([1, 0, 1, 0, 1]), "target": np.array([1, 0, 1, 0, 1])}, lazy=False
+        pl.DataFrame({"pred": [1, 0, 1, 0, 1], "target": [1, 0, 1, 0, 1]}), lazy=False
     )
     assert isinstance(result, Result)
     assert objects_are_equal(result.compute_metrics(), {"count": 5, "precision": 1.0})
@@ -51,17 +51,7 @@ def test_binary_precision_evaluator_evaluate_lazy_false() -> None:
 def test_binary_precision_evaluator_evaluate_missing_keys() -> None:
     assert (
         BinaryPrecisionEvaluator(y_true="target", y_pred="prediction")
-        .evaluate({"pred": np.array([1, 0, 0, 1, 1]), "target": np.array([1, 0, 1, 0, 1])})
-        .equal(EmptyResult())
-    )
-
-
-def test_binary_precision_evaluator_evaluate_lazy_false_missing_keys() -> None:
-    assert (
-        BinaryPrecisionEvaluator(y_true="target", y_pred="missing")
-        .evaluate(
-            {"pred": np.array([1, 0, 0, 1, 1]), "target": np.array([1, 0, 1, 0, 1])}, lazy=False
-        )
+        .evaluate(pl.DataFrame({"pred": [1, 0, 0, 1, 1], "target": [1, 0, 1, 0, 1]}))
         .equal(EmptyResult())
     )
 
@@ -74,5 +64,47 @@ def test_binary_precision_evaluator_evaluate_dataframe() -> None:
             BinaryPrecisionResult(
                 y_true=np.array([1, 0, 1, 0, 1]), y_pred=np.array([1, 0, 0, 1, 1])
             )
+        )
+    )
+
+
+def test_binary_precision_evaluator_evaluate_drop_nulls() -> None:
+    assert (
+        BinaryPrecisionEvaluator(y_true="target", y_pred="pred")
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            BinaryPrecisionResult(
+                y_true=np.array([1, 2, 3, 2, 1]), y_pred=np.array([3, 2, 0, 1, 0])
+            )
+        )
+    )
+
+
+def test_binary_precision_evaluator_evaluate_drop_nulls_false() -> None:
+    assert (
+        BinaryPrecisionEvaluator(y_true="target", y_pred="pred", drop_nulls=False)
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            BinaryPrecisionResult(
+                y_true=np.array([1.0, 2.0, 3.0, 2.0, 1.0, 2.0, float("nan"), float("nan")]),
+                y_pred=np.array([3.0, 2.0, 0.0, 1.0, 0.0, float("nan"), 1.0, float("nan")]),
+            ),
+            equal_nan=True,
         )
     )
