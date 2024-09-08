36a0ff53aeb6f404307370b5baff53fff58582da
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 14:45:06 -0700

    Improve `MulticlassRecallEvaluator`

--- tests/unit/evaluator/test_multiclass_recall.py
+++ tests/unit/evaluator/test_multiclass_recall.py
@@ -73,3 +73,45 @@ def test_multiclass_recall_evaluator_evaluate_lazy_false_missing_keys() -> None:
         )
         .equal(EmptyResult())
     )
+
+
+def test_multiclass_recall_evaluator_evaluate_drop_nulls() -> None:
+    assert (
+        MulticlassRecallEvaluator(y_true="target", y_pred="pred")
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            MulticlassRecallResult(
+                y_true=np.array([1, 2, 3, 2, 1]), y_pred=np.array([3, 2, 0, 1, 0])
+            )
+        )
+    )
+
+
+def test_multiclass_recall_evaluator_evaluate_drop_nulls_false() -> None:
+    assert (
+        MulticlassRecallEvaluator(y_true="target", y_pred="pred", drop_nulls=False)
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            MulticlassRecallResult(
+                y_true=np.array([1.0, 2.0, 3.0, 2.0, 1.0, 2.0, float("nan"), float("nan")]),
+                y_pred=np.array([3.0, 2.0, 0.0, 1.0, 0.0, float("nan"), 1.0, float("nan")]),
+            ),
+            equal_nan=True,
+        )
+    )
