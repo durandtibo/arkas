3cd0806c1a457e32a8ea2125d58fca5696f2c67b
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 14:49:55 -0700

    Improve `MulticlassJaccardEvaluator` (#324)

--- tests/unit/evaluator/test_multiclass_jaccard.py
+++ tests/unit/evaluator/test_multiclass_jaccard.py
@@ -85,3 +85,45 @@ def test_multiclass_jaccard_evaluator_evaluate_dataframe() -> None:
             )
         )
     )
+
+
+def test_multiclass_jaccard_evaluator_evaluate_drop_nulls() -> None:
+    assert (
+        MulticlassJaccardEvaluator(y_true="target", y_pred="pred")
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            MulticlassJaccardResult(
+                y_true=np.array([1, 2, 3, 2, 1]), y_pred=np.array([3, 2, 0, 1, 0])
+            )
+        )
+    )
+
+
+def test_multiclass_jaccard_evaluator_evaluate_drop_nulls_false() -> None:
+    assert (
+        MulticlassJaccardEvaluator(y_true="target", y_pred="pred", drop_nulls=False)
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [3, 2, 0, 1, 0, None, 1, None],
+                    "target": [1, 2, 3, 2, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            MulticlassJaccardResult(
+                y_true=np.array([1.0, 2.0, 3.0, 2.0, 1.0, 2.0, float("nan"), float("nan")]),
+                y_pred=np.array([3.0, 2.0, 0.0, 1.0, 0.0, float("nan"), 1.0, float("nan")]),
+            ),
+            equal_nan=True,
+        )
+    )
