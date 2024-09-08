4f5ff6f14fba33127690a5b6c70d9a725fd1213b
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 11:43:46 -0700

    Improve `BinaryRocAucEvaluator`

--- tests/unit/evaluator/test_binary_roc_auc.py
+++ tests/unit/evaluator/test_binary_roc_auc.py
@@ -26,7 +26,7 @@ def test_binary_roc_auc_evaluator_str() -> None:
 def test_binary_roc_auc_evaluator_evaluate() -> None:
     assert (
         BinaryRocAucEvaluator(y_true="target", y_score="pred")
-        .evaluate({"pred": np.array([2, -1, 0, 3, 1]), "target": np.array([1, 0, 0, 1, 1])})
+        .evaluate(pl.DataFrame({"pred": [2, -1, 0, 3, 1], "target": [1, 0, 0, 1, 1]}))
         .equal(
             BinaryRocAucResult(y_true=np.array([1, 0, 0, 1, 1]), y_score=np.array([2, -1, 0, 3, 1]))
         )
@@ -36,9 +36,7 @@ def test_binary_roc_auc_evaluator_evaluate() -> None:
 def test_binary_roc_auc_evaluator_evaluate_lazy_false() -> None:
     assert (
         BinaryRocAucEvaluator(y_true="target", y_score="pred")
-        .evaluate(
-            {"pred": np.array([2, -1, 0, 3, 1]), "target": np.array([1, 0, 0, 1, 1])}, lazy=False
-        )
+        .evaluate(pl.DataFrame({"pred": [2, -1, 0, 3, 1], "target": [1, 0, 0, 1, 1]}), lazy=False)
         .equal(Result(metrics={"count": 5, "roc_auc": 1.0}))
     )
 
@@ -46,7 +44,7 @@ def test_binary_roc_auc_evaluator_evaluate_lazy_false() -> None:
 def test_binary_roc_auc_evaluator_evaluate_missing_keys() -> None:
     assert (
         BinaryRocAucEvaluator(y_true="target", y_score="prediction")
-        .evaluate({"pred": np.array([2, -1, 0, 3, 1]), "target": np.array([1, 0, 0, 1, 1])})
+        .evaluate(pl.DataFrame({"pred": [2, -1, 0, 3, 1], "target": [1, 0, 1, 0, 1]}))
         .equal(EmptyResult())
     )
 
@@ -54,18 +52,46 @@ def test_binary_roc_auc_evaluator_evaluate_missing_keys() -> None:
 def test_binary_roc_auc_evaluator_evaluate_lazy_false_missing_keys() -> None:
     assert (
         BinaryRocAucEvaluator(y_true="target", y_score="missing")
-        .evaluate(
-            {"pred": np.array([2, -1, 0, 3, 1]), "target": np.array([1, 0, 0, 1, 1])}, lazy=False
-        )
+        .evaluate(pl.DataFrame({"pred": [2, -1, 0, 3, 1], "target": [1, 0, 1, 0, 1]}), lazy=False)
         .equal(EmptyResult())
     )
 
 
-def test_binary_roc_auc_evaluator_evaluate_dataframe() -> None:
+def test_binary_roc_auc_evaluator_evaluate_drop_nulls() -> None:
     assert (
         BinaryRocAucEvaluator(y_true="target", y_score="pred")
-        .evaluate(pl.DataFrame({"pred": [2, -1, 0, 3, 1], "target": [1, 0, 1, 0, 1]}))
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [2, -1, 0, 3, 1, None, 1, None],
+                    "target": [1, 0, 1, 0, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
         .equal(
             BinaryRocAucResult(y_true=np.array([1, 0, 1, 0, 1]), y_score=np.array([2, -1, 0, 3, 1]))
+        )
+    )
+
+
+def test_binary_roc_auc_evaluator_evaluate_drop_nulls_false() -> None:
+    assert (
+        BinaryRocAucEvaluator(y_true="target", y_score="pred", drop_nulls=False)
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [2, -1, 0, 3, 1, None, 1, None],
+                    "target": [1, 0, 1, 0, 1, 2, None, None],
+                    "col": [1, None, 3, 4, 5, None, 7, None],
+                }
+            )
+        )
+        .equal(
+            BinaryRocAucResult(
+                y_true=np.array([1.0, 0.0, 1.0, 0.0, 1.0, 2.0, float("nan"), float("nan")]),
+                y_score=np.array([2.0, -1.0, 0.0, 3.0, 1.0, float("nan"), 1.0, float("nan")]),
+            ),
+            equal_nan=True,
         )
     )
