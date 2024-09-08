b07273b220b7cb3f2c47c6348a3ba1444ec11abc
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 15:30:41 -0700

    Improve `MultilabelRecallEvaluator`

--- src/arkas/evaluator/multilabel_recall.py
+++ src/arkas/evaluator/multilabel_recall.py
@@ -7,26 +7,26 @@ __all__ = ["MultilabelRecallEvaluator"]
 import logging
 from typing import TYPE_CHECKING
 
-from arkas.evaluator.base import BaseLazyEvaluator
-from arkas.result import EmptyResult, MultilabelRecallResult
+from arkas.evaluator.lazy import BaseLazyEvaluator
+from arkas.result import MultilabelRecallResult, Result
 from arkas.utils.array import to_array
-from arkas.utils.data import find_keys, find_missing_keys
 
 if TYPE_CHECKING:
     import polars as pl
 
-    from arkas.result import BaseResult
 
 logger = logging.getLogger(__name__)
 
 
-class MultilabelRecallEvaluator(BaseLazyEvaluator):
+class MultilabelRecallEvaluator(BaseLazyEvaluator[MultilabelRecallResult]):
     r"""Implement the recall evaluator for multilabel labels.
 
     Args:
         y_true: The key or column name of the ground truth target
             labels.
         y_pred: The key or column name of the predicted labels.
+        drop_nulls: If ``True``, the rows with null values in
+            ``y_true`` or ``y_pred`` columns are dropped.
 
     Example usage:
 
@@ -36,7 +36,7 @@ class MultilabelRecallEvaluator(BaseLazyEvaluator):
     >>> from arkas.evaluator import MultilabelRecallEvaluator
     >>> evaluator = MultilabelRecallEvaluator(y_true="target", y_pred="pred")
     >>> evaluator
-    MultilabelRecallEvaluator(y_true=target, y_pred=pred)
+    MultilabelRecallEvaluator(y_true=target, y_pred=pred, drop_nulls=True)
     >>> data = pl.DataFrame(
     ...     {
     ...         "pred": [[1, 0, 0], [0, 1, 1], [0, 1, 1], [1, 0, 0], [1, 0, 0]],
@@ -51,25 +51,28 @@ class MultilabelRecallEvaluator(BaseLazyEvaluator):
     ```
     """
 
-    def __init__(self, y_true: str, y_pred: str) -> None:
+    def __init__(self, y_true: str, y_pred: str, drop_nulls: bool = True) -> None:
+        super().__init__(drop_nulls=drop_nulls)
         self._y_true = y_true
         self._y_pred = y_pred
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(y_true={self._y_true}, y_pred={self._y_pred})"
-
-    def _evaluate(self, data: dict | pl.DataFrame) -> BaseResult:
-        logger.info(
-            f"Evaluating the multilabel recall | y_true={self._y_true} | y_pred={self._y_pred}"
+        return (
+            f"{self.__class__.__qualname__}(y_true={self._y_true}, y_pred={self._y_pred}, "
+            f"drop_nulls={self._drop_nulls})"
         )
-        if missing_keys := find_missing_keys(
-            keys=find_keys(data), queries=[self._y_pred, self._y_true]
-        ):
-            logger.warning(
-                "Skipping the multilabel recall evaluation because some keys are missing: "
-                f"{sorted(missing_keys)}"
-            )
-            return EmptyResult()
+
+    def evaluate(self, data: pl.DataFrame, lazy: bool = True) -> MultilabelRecallResult | Result:
+        logger.info(
+            f"Evaluating the multilabel recall | y_true={self._y_true} | "
+            f"y_pred={self._y_pred} | drop_nulls={self._drop_nulls}"
+        )
+        return self._evaluate(data, lazy)
+
+    def _compute_result(self, data: pl.DataFrame) -> MultilabelRecallResult:
         return MultilabelRecallResult(
             y_true=to_array(data[self._y_true]), y_pred=to_array(data[self._y_pred])
         )
+
+    def _get_columns(self) -> tuple[str, ...]:
+        return (self._y_true, self._y_pred)
