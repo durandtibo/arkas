6ed501be5f90c96ec0467e1bda4aa762382633d7
Thibaut Durand <durand.tibo@gmail.com>
2024-09-08 23:40:12 -0700

    Remove old `BaseLazyEvaluator`

--- src/arkas/evaluator/__init__.py
+++ src/arkas/evaluator/__init__.py
@@ -40,12 +40,7 @@ __all__ = [
 from arkas.evaluator.accuracy import AccuracyEvaluator
 from arkas.evaluator.ap import AveragePrecisionEvaluator
 from arkas.evaluator.balanced_accuracy import BalancedAccuracyEvaluator
-from arkas.evaluator.base import (
-    BaseEvaluator,
-    BaseLazyEvaluator,
-    is_evaluator_config,
-    setup_evaluator,
-)
+from arkas.evaluator.base import BaseEvaluator, is_evaluator_config, setup_evaluator
 from arkas.evaluator.binary_ap import BinaryAveragePrecisionEvaluator
 from arkas.evaluator.binary_classification import BinaryClassificationEvaluator
 from arkas.evaluator.binary_confmat import BinaryConfusionMatrixEvaluator
@@ -54,6 +49,7 @@ from arkas.evaluator.binary_jaccard import BinaryJaccardEvaluator
 from arkas.evaluator.binary_precision import BinaryPrecisionEvaluator
 from arkas.evaluator.binary_recall import BinaryRecallEvaluator
 from arkas.evaluator.binary_roc_auc import BinaryRocAucEvaluator
+from arkas.evaluator.lazy import BaseLazyEvaluator
 from arkas.evaluator.mae import MeanAbsoluteErrorEvaluator
 from arkas.evaluator.mapping import MappingEvaluator
 from arkas.evaluator.multiclass_ap import MulticlassAveragePrecisionEvaluator
