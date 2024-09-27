7359783fd8993e583b055e972c1aaf9c4ed0294e
Thibaut Durand <durand.tibo@gmail.com>
2024-09-26 22:40:19 -0700

    Add `is_hydra_available` (#362)

--- tests/unit/utils/test_imports.py
+++ tests/unit/utils/test_imports.py
@@ -7,11 +7,14 @@ import pytest
 from arkas.utils.imports import (
     check_colorlog,
     check_hya,
+    check_hydra,
     check_markdown,
     colorlog_available,
     hya_available,
+    hydra_available,
     is_colorlog_available,
     is_hya_available,
+    is_hydra_available,
     is_markdown_available,
     markdown_available,
 )
@@ -123,6 +126,60 @@ def test_hya_available_decorator_without_package() -> None:
     with patch("arkas.utils.imports.is_hya_available", lambda: False):
 
         @hya_available
+        def fn(n: int = 0) -> int:
+            return 42 + n
+
+        assert fn(2) is None
+
+
+#################
+#     hydra     #
+#################
+
+
+def test_check_hydra_with_package() -> None:
+    with patch("arkas.utils.imports.is_hydra_available", lambda: True):
+        check_hydra()
+
+
+def test_check_hydra_without_package() -> None:
+    with (
+        patch("arkas.utils.imports.is_hydra_available", lambda: False),
+        pytest.raises(RuntimeError, match="'hydra' package is required but not installed."),
+    ):
+        check_hydra()
+
+
+def test_is_hydra_available() -> None:
+    assert isinstance(is_hydra_available(), bool)
+
+
+def test_hydra_available_with_package() -> None:
+    with patch("arkas.utils.imports.is_hydra_available", lambda: True):
+        fn = hydra_available(my_function)
+        assert fn(2) == 44
+
+
+def test_hydra_available_without_package() -> None:
+    with patch("arkas.utils.imports.is_hydra_available", lambda: False):
+        fn = hydra_available(my_function)
+        assert fn(2) is None
+
+
+def test_hydra_available_decorator_with_package() -> None:
+    with patch("arkas.utils.imports.is_hydra_available", lambda: True):
+
+        @hydra_available
+        def fn(n: int = 0) -> int:
+            return 42 + n
+
+        assert fn(2) == 44
+
+
+def test_hydra_available_decorator_without_package() -> None:
+    with patch("arkas.utils.imports.is_hydra_available", lambda: False):
+
+        @hydra_available
         def fn(n: int = 0) -> int:
             return 42 + n
 
