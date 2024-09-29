fb70f6bbb637bc24226e08eece430cef4a0c6b84
Thibaut Durand <durand.tibo@gmail.com>
2024-09-28 22:01:06 -0700

    Add integration tests for run entrypoint

---
+++ tests/integration/test_run.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import logging
+import subprocess
+from typing import TYPE_CHECKING
+
+import pytest
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def run_bash_command(cmd: str) -> None:
+    r"""Execute a bash command.
+
+    Args:
+        cmd: The command to run.
+    """
+    logger.info(f"execute the following command: {cmd}")
+    subprocess.run(cmd.split(), check=True)  # noqa: S603
+
+
+def test_run_cli_successful(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("successful")
+    run_bash_command(
+        f"python -m arkas.cli.run -cd=conf/demo -cn=toy_binary_classification exp_dir={path}"
+    )
+    assert path.joinpath("metrics.pkl").is_file()
+
+
+def test_run_cli_error(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("unsuccessful")
+    with pytest.raises(subprocess.CalledProcessError):
+        run_bash_command(
+            r"python -m arkas.cli.run -cd=conf/demo -cn=toy_binary_classification "
+            rf"exp_dir={path} ingestor.path={tmp_path}"
+        )
+    assert not tmp_path.joinpath("metrics.pkl").is_file()
