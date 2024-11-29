9abb76100538ab06ba20aefb1e03d168ead03893
Thibaut Durand <durand.tibo@gmail.com>
2024-11-29 03:39:05 -0800

    Reorganize correlation metrics

--- src/arkas/result/pearson.py
+++ src/arkas/result/pearson.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING, Any
 
 from coola import objects_are_equal
 
-from arkas.metric.regression.pearson import pearsonr
+from arkas.metric.correlation.pearson import pearsonr
 from arkas.metric.utils import check_same_shape_pred
 from arkas.result.base import BaseResult
 
