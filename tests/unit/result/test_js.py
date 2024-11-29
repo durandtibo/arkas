eff0e3da1462c499dc892841c55e7216a491a6aa
Thibaut Durand <durand.tibo@gmail.com>
2024-11-29 06:33:39 -0800

    Rename `JSDivResult`

--- tests/unit/result/test_js.py
+++ tests/unit/result/test_js.py
@@ -11,76 +11,76 @@ from arkas.result import JSDivResult
 #################################
 
 
-def test_jensen_shannon_divergence_result_p() -> None:
+def test_js_div_result_p() -> None:
     assert objects_are_allclose(
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.2, 0.5, 0.2, 0.1])).p,
         np.array([0.1, 0.6, 0.1, 0.2]),
     )
 
 
-def test_jensen_shannon_divergence_result_p_2d() -> None:
+def test_js_div_result_p_2d() -> None:
     assert objects_are_allclose(
         JSDivResult(p=np.array([[0.1, 0.6], [0.1, 0.2]]), q=np.array([[0.2, 0.5], [0.2, 0.1]])).p,
         np.array([0.1, 0.6, 0.1, 0.2]),
     )
 
 
-def test_jensen_shannon_divergence_result_q() -> None:
+def test_js_div_result_q() -> None:
     assert objects_are_allclose(
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.2, 0.5, 0.2, 0.1])).q,
         np.array([0.2, 0.5, 0.2, 0.1]),
     )
 
 
-def test_jensen_shannon_divergence_result_q_2d() -> None:
+def test_js_div_result_q_2d() -> None:
     assert objects_are_allclose(
         JSDivResult(p=np.array([[0.1, 0.6], [0.1, 0.2]]), q=np.array([[0.2, 0.5], [0.2, 0.1]])).q,
         np.array([0.2, 0.5, 0.2, 0.1]),
     )
 
 
-def test_jensen_shannon_divergence_result_y_incorrect_shape() -> None:
+def test_js_div_result_y_incorrect_shape() -> None:
     with pytest.raises(RuntimeError, match="arrays have different shapes"):
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2, 0]))
 
 
-def test_jensen_shannon_divergence_result_repr() -> None:
+def test_js_div_result_repr() -> None:
     assert repr(
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     ).startswith("JSDivResult(")
 
 
-def test_jensen_shannon_divergence_result_str() -> None:
+def test_js_div_result_str() -> None:
     assert str(
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     ).startswith("JSDivResult(")
 
 
-def test_jensen_shannon_divergence_result_equal_true() -> None:
+def test_js_div_result_equal_true() -> None:
     assert JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2])).equal(
         JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     )
 
 
-def test_jensen_shannon_divergence_result_equal_false_different_p() -> None:
+def test_js_div_result_equal_false_different_p() -> None:
     assert not JSDivResult(
         p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2])
     ).equal(JSDivResult(p=np.array([0.1, 0.6, 0.2, 0.1]), q=np.array([0.1, 0.6, 0.1, 0.2])))
 
 
-def test_jensen_shannon_divergence_result_equal_false_different_q() -> None:
+def test_js_div_result_equal_false_different_q() -> None:
     assert not JSDivResult(
         p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2])
     ).equal(JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.2, 0.5, 0.2, 0.1])))
 
 
-def test_jensen_shannon_divergence_result_equal_false_different_type() -> None:
+def test_js_div_result_equal_false_different_type() -> None:
     assert not JSDivResult(
         p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2])
     ).equal(42)
 
 
-def test_jensen_shannon_divergence_result_equal_nan_true() -> None:
+def test_js_div_result_equal_nan_true() -> None:
     assert JSDivResult(
         p=np.array([1, 0, 0, 0, float("nan")]), q=np.array([1, 0, 0, float("nan"), 0])
     ).equal(
@@ -92,7 +92,7 @@ def test_jensen_shannon_divergence_result_equal_nan_true() -> None:
     )
 
 
-def test_jensen_shannon_divergence_result_equal_nan_false() -> None:
+def test_js_div_result_equal_nan_false() -> None:
     assert not JSDivResult(
         p=np.array([1, 0, 0, 0, float("nan")]), q=np.array([1, 0, 0, float("nan"), 0])
     ).equal(
@@ -103,7 +103,7 @@ def test_jensen_shannon_divergence_result_equal_nan_false() -> None:
     )
 
 
-def test_jensen_shannon_divergence_result_compute_metrics_same() -> None:
+def test_js_div_result_compute_metrics_same() -> None:
     result = JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     assert objects_are_allclose(
         result.compute_metrics(),
@@ -111,7 +111,7 @@ def test_jensen_shannon_divergence_result_compute_metrics_same() -> None:
     )
 
 
-def test_jensen_shannon_divergence_result_compute_metrics_different() -> None:
+def test_js_div_result_compute_metrics_different() -> None:
     result = JSDivResult(p=np.array([0.10, 0.40, 0.50]), q=np.array([0.80, 0.15, 0.05]))
     assert objects_are_allclose(
         result.compute_metrics(),
@@ -119,7 +119,7 @@ def test_jensen_shannon_divergence_result_compute_metrics_different() -> None:
     )
 
 
-def test_jensen_shannon_divergence_result_compute_metrics_empty() -> None:
+def test_js_div_result_compute_metrics_empty() -> None:
     result = JSDivResult(p=np.array([]), q=np.array([]))
     assert objects_are_allclose(
         result.compute_metrics(),
@@ -128,7 +128,7 @@ def test_jensen_shannon_divergence_result_compute_metrics_empty() -> None:
     )
 
 
-def test_jensen_shannon_divergence_result_compute_metrics_prefix_suffix() -> None:
+def test_js_div_result_compute_metrics_prefix_suffix() -> None:
     result = JSDivResult(
         p=np.array([0.1, 0.6, 0.1, 0.2]),
         q=np.array([0.1, 0.6, 0.1, 0.2]),
@@ -139,16 +139,16 @@ def test_jensen_shannon_divergence_result_compute_metrics_prefix_suffix() -> Non
     )
 
 
-def test_jensen_shannon_divergence_result_generate_figures() -> None:
+def test_js_div_result_generate_figures() -> None:
     result = JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     assert objects_are_allclose(result.generate_figures(), {})
 
 
-def test_jensen_shannon_divergence_result_generate_figures_empty() -> None:
+def test_js_div_result_generate_figures_empty() -> None:
     result = JSDivResult(p=np.array([]), q=np.array([]))
     assert objects_are_allclose(result.generate_figures(), {})
 
 
-def test_jensen_shannon_divergence_result_generate_figures_prefix_suffix() -> None:
+def test_js_div_result_generate_figures_prefix_suffix() -> None:
     result = JSDivResult(p=np.array([0.1, 0.6, 0.1, 0.2]), q=np.array([0.1, 0.6, 0.1, 0.2]))
     assert objects_are_allclose(result.generate_figures(prefix="prefix_", suffix="_suffix"), {})
