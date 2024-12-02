d38e15021cd3ff9642cc7aa86b68c225ea4a1fa2
Thibaut Durand <durand.tibo@gmail.com>
2024-12-01 18:56:06 -0800

    Improve `fbeta_score` (#479)

--- src/arkas/metric/classification/fbeta.py
+++ src/arkas/metric/classification/fbeta.py
@@ -9,7 +9,7 @@ __all__ = [
     "multilabel_fbeta_score",
 ]
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import numpy as np
 from sklearn import metrics
@@ -23,7 +23,7 @@ from arkas.metric.utils import (
 )
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Sequence
+    from collections.abc import Sequence
 
 
 def fbeta_score(
@@ -170,15 +170,20 @@ def binary_fbeta_score(
 
     ```
     """
-    return _eval_all(
-        fn=_binary_fbeta_score,
-        y_true=y_true.ravel(),
-        y_pred=y_pred.ravel(),
-        betas=betas,
-        prefix=prefix,
-        suffix=suffix,
-        nan_policy=nan_policy,
+    y_true, y_pred = preprocess_pred(
+        y_true=y_true.ravel(), y_pred=y_pred.ravel(), drop_nan=nan_policy == "omit"
     )
+    y_true_nan = contains_nan(arr=y_true, nan_policy=nan_policy, name="'y_true'")
+    y_pred_nan = contains_nan(arr=y_pred, nan_policy=nan_policy, name="'y_pred'")
+
+    count = y_true.size
+    out = {f"{prefix}count{suffix}": count}
+    for beta in betas:
+        score = float("nan")
+        if count > 0 and not y_true_nan and not y_pred_nan:
+            score = float(metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta))
+        out[f"{prefix}f{beta}{suffix}"] = score
+    return out
 
 
 def multiclass_fbeta_score(
@@ -225,15 +230,39 @@ def multiclass_fbeta_score(
 
     ```
     """
-    return _eval_all(
-        fn=_multiclass_fbeta_score,
-        y_true=y_true.ravel(),
-        y_pred=y_pred.ravel(),
-        betas=betas,
-        prefix=prefix,
-        suffix=suffix,
-        nan_policy=nan_policy,
+    y_true, y_pred = preprocess_pred(
+        y_true=y_true.ravel(), y_pred=y_pred.ravel(), drop_nan=nan_policy == "omit"
     )
+    y_true_nan = contains_nan(arr=y_true, nan_policy=nan_policy, name="'y_true'")
+    y_pred_nan = contains_nan(arr=y_pred, nan_policy=nan_policy, name="'y_pred'")
+
+    n_samples = y_true.shape[0]
+    out = {f"{prefix}count{suffix}": n_samples}
+    for beta in betas:
+        fbeta = np.array([])
+        macro, micro, weighted = float("nan"), float("nan"), float("nan")
+        if n_samples > 0 and not y_true_nan and not y_pred_nan:
+            macro = metrics.fbeta_score(
+                y_true=y_true, y_pred=y_pred, beta=beta, average="macro", zero_division=0.0
+            )
+            micro = metrics.fbeta_score(
+                y_true=y_true, y_pred=y_pred, beta=beta, average="micro", zero_division=0.0
+            )
+            weighted = metrics.fbeta_score(
+                y_true=y_true, y_pred=y_pred, beta=beta, average="weighted", zero_division=0.0
+            )
+            fbeta = np.asarray(
+                metrics.fbeta_score(
+                    y_true=y_true, y_pred=y_pred, beta=beta, average=None, zero_division=0.0
+                )
+            ).ravel()
+        out |= {
+            f"{prefix}f{beta}{suffix}": fbeta,
+            f"{prefix}macro_f{beta}{suffix}": float(macro),
+            f"{prefix}micro_f{beta}{suffix}": float(micro),
+            f"{prefix}weighted_f{beta}{suffix}": float(weighted),
+        }
+    return out
 
 
 def multilabel_fbeta_score(
@@ -280,192 +309,33 @@ def multilabel_fbeta_score(
 
     ```
     """
-    return _eval_all(
-        fn=_multilabel_fbeta_score,
-        y_true=y_true,
-        y_pred=y_pred,
-        betas=betas,
-        prefix=prefix,
-        suffix=suffix,
-        nan_policy=nan_policy,
-    )
-
-
-def _binary_fbeta_score(
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
-    *,
-    beta: float = 1,
-    prefix: str = "",
-    suffix: str = "",
-    nan_policy: str = "propagate",
-) -> dict[str, float]:
-    r"""Return the F-beta metrics for binary labels.
-
-    Args:
-        y_true: The ground truth target labels. This input must
-            be an array of shape ``(n_samples,)``.
-        y_pred: The predicted labels. This input must
-            be an array of shape ``(n_samples,)``.
-        beta: The beta used to compute the F-beta score.
-        prefix: The key prefix in the returned dictionary.
-        suffix: The key suffix in the returned dictionary.
-        nan_policy: The policy on how to handle NaN values in the input
-            arrays. The following options are available: ``'omit'``,
-            ``'propagate'``, and ``'raise'``.
-
-    Returns:
-        The computed metrics.
-    """
-    y_true, y_pred = preprocess_pred(
-        y_true=y_true.ravel(), y_pred=y_pred.ravel(), drop_nan=nan_policy == "omit"
-    )
-    y_true_nan = contains_nan(arr=y_true, nan_policy=nan_policy, name="'y_true'")
-    y_pred_nan = contains_nan(arr=y_pred, nan_policy=nan_policy, name="'y_pred'")
-
-    count, fbeta = y_true.size, float("nan")
-    if count > 0 and not y_true_nan and not y_pred_nan:
-        fbeta = float(metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta))
-    return {f"{prefix}count{suffix}": count, f"{prefix}f{beta}{suffix}": fbeta}
-
-
-def _multiclass_fbeta_score(
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
-    *,
-    beta: float = 1,
-    prefix: str = "",
-    suffix: str = "",
-    nan_policy: str = "propagate",
-) -> dict[str, float | np.ndarray]:
-    r"""Return the F-beta metrics for multiclass labels.
-
-    Args:
-        y_true: The ground truth target labels. This input must
-            be an array of shape ``(n_samples,)``.
-        y_pred: The predicted labels. This input must
-            be an array of shape ``(n_samples,)``.
-        beta: The beta used to compute the F-beta score.
-        prefix: The key prefix in the returned dictionary.
-        suffix: The key suffix in the returned dictionary.
-        nan_policy: The policy on how to handle NaN values in the input
-            arrays. The following options are available: ``'omit'``,
-            ``'propagate'``, and ``'raise'``.
-
-    Returns:
-        The computed metrics.
-    """
-    y_true, y_pred = preprocess_pred(
-        y_true=y_true.ravel(), y_pred=y_pred.ravel(), drop_nan=nan_policy == "omit"
-    )
-    y_true_nan = contains_nan(arr=y_true, nan_policy=nan_policy, name="'y_true'")
-    y_pred_nan = contains_nan(arr=y_pred, nan_policy=nan_policy, name="'y_pred'")
-
-    fbeta = np.array([])
-    macro, micro, weighted = float("nan"), float("nan"), float("nan")
-    n_samples = y_true.shape[0]
-    if n_samples > 0 and not y_true_nan and not y_pred_nan:
-        macro = metrics.fbeta_score(
-            y_true=y_true, y_pred=y_pred, beta=beta, average="macro", zero_division=0.0
-        )
-        micro = metrics.fbeta_score(
-            y_true=y_true, y_pred=y_pred, beta=beta, average="micro", zero_division=0.0
-        )
-        weighted = metrics.fbeta_score(
-            y_true=y_true, y_pred=y_pred, beta=beta, average="weighted", zero_division=0.0
-        )
-        fbeta = np.asarray(
-            metrics.fbeta_score(
-                y_true=y_true, y_pred=y_pred, beta=beta, average=None, zero_division=0.0
-            )
-        ).ravel()
-    return {
-        f"{prefix}count{suffix}": n_samples,
-        f"{prefix}f{beta}{suffix}": fbeta,
-        f"{prefix}macro_f{beta}{suffix}": float(macro),
-        f"{prefix}micro_f{beta}{suffix}": float(micro),
-        f"{prefix}weighted_f{beta}{suffix}": float(weighted),
-    }
-
-
-def _multilabel_fbeta_score(
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
-    *,
-    beta: float = 1,
-    prefix: str = "",
-    suffix: str = "",
-    nan_policy: str = "propagate",
-) -> dict[str, float | np.ndarray]:
-    r"""Return the fbeta metrics for multilabel labels.
-
-    Args:
-        y_true: The ground truth target labels. This input must
-            be an array of shape ``(n_samples, n_classes)``.
-        y_pred: The predicted labels. This input must
-            be an array of shape ``(n_samples, n_classes)``.
-        beta: The beta used to compute the F-beta score.
-        prefix: The key prefix in the returned dictionary.
-        suffix: The key suffix in the returned dictionary.
-        nan_policy: The policy on how to handle NaN values in the input
-            arrays. The following options are available: ``'omit'``,
-            ``'propagate'``, and ``'raise'``.
-
-    Returns:
-        The computed metrics.
-    """
     y_true, y_pred = preprocess_pred_multilabel(y_true, y_pred, drop_nan=nan_policy == "omit")
     y_true_nan = contains_nan(arr=y_true, nan_policy=nan_policy, name="'y_true'")
     y_pred_nan = contains_nan(arr=y_pred, nan_policy=nan_policy, name="'y_pred'")
 
-    fbeta = np.array([])
-    macro, micro, weighted = float("nan"), float("nan"), float("nan")
     n_samples = y_true.shape[0]
-    if n_samples > 0 and not y_true_nan and not y_pred_nan:
-        fbeta = np.array(
-            metrics.fbeta_score(
-                y_true=y_true,
-                y_pred=y_pred,
-                beta=beta,
-                average="binary" if y_pred.shape[1] == 1 else None,
-            )
-        ).ravel()
-        macro = metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta, average="macro")
-        micro = metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta, average="micro")
-        weighted = metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta, average="weighted")
-    return {
-        f"{prefix}count{suffix}": n_samples,
-        f"{prefix}f{beta}{suffix}": fbeta,
-        f"{prefix}macro_f{beta}{suffix}": float(macro),
-        f"{prefix}micro_f{beta}{suffix}": float(micro),
-        f"{prefix}weighted_f{beta}{suffix}": float(weighted),
-    }
-
-
-def _eval_all(
-    fn: Callable,
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
-    betas: Sequence[float],
-    **kwargs: Any,
-) -> dict[str, float | np.ndarray]:
-    r"""Evaluate the function for all the betas and merge the results in
-    a single dictionary.
-
-    Args:
-        fn: The function to evaluate.
-        y_true: The ground truth target labels. This input must
-            be an array of shape ``(n_samples,)`` or
-            ``(n_samples, n_classes)``.
-        y_pred: The predicted labels. This input must be an array of
-            shape ``(n_samples,)`` or ``(n_samples, n_classes)``.
-        betas: The betas used to compute the F-beta scores.
-        kwargs: Keyword arguments.
-
-    Returns:
-        The computed metrics.
-    """
-    out = {}
+    out = {f"{prefix}count{suffix}": n_samples}
     for beta in betas:
-        out |= fn(y_true=y_true, y_pred=y_pred, beta=beta, **kwargs)
+        fbeta = np.array([])
+        macro, micro, weighted = float("nan"), float("nan"), float("nan")
+        if n_samples > 0 and not y_true_nan and not y_pred_nan:
+            fbeta = np.array(
+                metrics.fbeta_score(
+                    y_true=y_true,
+                    y_pred=y_pred,
+                    beta=beta,
+                    average="binary" if y_pred.shape[1] == 1 else None,
+                )
+            ).ravel()
+            macro = metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta, average="macro")
+            micro = metrics.fbeta_score(y_true=y_true, y_pred=y_pred, beta=beta, average="micro")
+            weighted = metrics.fbeta_score(
+                y_true=y_true, y_pred=y_pred, beta=beta, average="weighted"
+            )
+        out |= {
+            f"{prefix}f{beta}{suffix}": fbeta,
+            f"{prefix}macro_f{beta}{suffix}": float(macro),
+            f"{prefix}micro_f{beta}{suffix}": float(micro),
+            f"{prefix}weighted_f{beta}{suffix}": float(weighted),
+        }
     return out
