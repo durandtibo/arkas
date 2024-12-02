ba536d5f655a1b769610be2db15bc17c4bd5ba7a
Thibaut Durand <durand.tibo@gmail.com>
2024-12-01 16:55:47 -0800

    Add missing tests (#473)

--- tests/unit/metric/classification/test_roc_auc.py
+++ tests/unit/metric/classification/test_roc_auc.py
@@ -46,6 +46,40 @@ def test_roc_auc_binary_prefix_suffix() -> None:
     )
 
 
+def test_roc_auc_binary_nan_omit() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([1, 0, 0, 1, float("nan")]),
+            y_score=np.array([float("nan"), -1, 0, 3, 1]),
+            label_type="binary",
+            nan_policy="omit",
+        ),
+        {"count": 3, "roc_auc": 1.0},
+    )
+
+
+def test_roc_auc_binary_nan_propagate() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([1, 0, 0, 1, float("nan")]),
+            y_score=np.array([float("nan"), -1, 0, 3, 1]),
+            label_type="binary",
+        ),
+        {"count": 5, "roc_auc": float("nan")},
+        equal_nan=True,
+    )
+
+
+def test_roc_auc_binary_nan_raise() -> None:
+    with pytest.raises(ValueError, match="'y_true' contains at least one NaN value"):
+        roc_auc(
+            y_true=np.array([1, 0, 0, 1, float("nan")]),
+            y_score=np.array([float("nan"), -1, 0, 3, 1]),
+            label_type="binary",
+            nan_policy="raise",
+        )
+
+
 def test_roc_auc_auto_multiclass() -> None:
     assert objects_are_equal(
         roc_auc(
@@ -125,6 +159,82 @@ def test_roc_auc_multiclass_prefix_suffix() -> None:
     )
 
 
+def test_roc_multiclass_auc_nan_omit() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([0, 0, 1, 1, 2, 2, float("nan")]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, float("nan")],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [float("nan"), float("nan"), float("nan")],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+            nan_policy="omit",
+        ),
+        {
+            "count": 4,
+            "macro_roc_auc": 1.0,
+            "micro_roc_auc": 1.0,
+            "roc_auc": np.array([1.0, 1.0, 1.0]),
+            "weighted_roc_auc": 1.0,
+        },
+    )
+
+
+def test_roc_auc_multiclass_nan_propagate() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([0, 0, 1, 1, 2, 2, float("nan")]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, float("nan")],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [float("nan"), float("nan"), float("nan")],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+        ),
+        {
+            "count": 7,
+            "macro_roc_auc": float("nan"),
+            "micro_roc_auc": float("nan"),
+            "roc_auc": np.array([]),
+            "weighted_roc_auc": float("nan"),
+        },
+        equal_nan=True,
+    )
+
+
+def test_roc_multiclass_auc_nan_raise() -> None:
+    with pytest.raises(ValueError, match="'y_true' contains at least one NaN value"):
+        roc_auc(
+            y_true=np.array([0, 0, 1, 1, 2, 2, float("nan")]),
+            y_score=np.array(
+                [
+                    [0.7, 0.2, 0.1],
+                    [0.4, 0.3, float("nan")],
+                    [0.1, 0.8, 0.1],
+                    [0.2, 0.5, 0.3],
+                    [0.3, 0.2, 0.5],
+                    [float("nan"), float("nan"), float("nan")],
+                    [0.7, 0.2, 0.1],
+                ]
+            ),
+            label_type="multiclass",
+            nan_policy="raise",
+        )
+
+
 def test_roc_auc_auto_multilabel() -> None:
     assert objects_are_allclose(
         roc_auc(
@@ -175,6 +285,58 @@ def test_roc_auc_multilabel_prefix_suffix() -> None:
             "prefix_weighted_roc_auc_suffix": 1.0,
         },
     )
+
+
+def test_roc_auc_multilabel_nan_omit() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, float("nan")]]),
+            y_score=np.array(
+                [[float("nan"), -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]
+            ),
+            label_type="multilabel",
+            nan_policy="omit",
+        ),
+        {
+            "count": 3,
+            "macro_roc_auc": 1.0,
+            "micro_roc_auc": 1.0,
+            "roc_auc": np.array([1.0, 1.0, 1.0]),
+            "weighted_roc_auc": 1.0,
+        },
+    )
+
+
+def test_roc_auc_multilabel_nan_propagate() -> None:
+    assert objects_are_equal(
+        roc_auc(
+            y_true=np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, float("nan")]]),
+            y_score=np.array(
+                [[float("nan"), -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]
+            ),
+            label_type="multilabel",
+        ),
+        {
+            "count": 5,
+            "macro_roc_auc": float("nan"),
+            "micro_roc_auc": float("nan"),
+            "roc_auc": np.array([]),
+            "weighted_roc_auc": float("nan"),
+        },
+        equal_nan=True,
+    )
+
+
+def test_roc_multilabel_auc_nan_raise() -> None:
+    with pytest.raises(ValueError, match="'y_true' contains at least one NaN value"):
+        roc_auc(
+            y_true=np.array([[1, 0, 1], [0, 1, 0], [0, 1, 0], [1, 0, 1], [1, 0, float("nan")]]),
+            y_score=np.array(
+                [[float("nan"), -1, 1], [-1, 1, -2], [0, 2, -3], [3, -2, 4], [1, -3, 5]]
+            ),
+            label_type="multilabel",
+            nan_policy="raise",
+        )
 
 
 def test_roc_auc_label_type_incorrect() -> None:
