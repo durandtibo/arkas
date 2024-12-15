0412a845876e0962e16ae15930ddc5079e21b6a3
Thibaut Durand <durand.tibo@gmail.com>
2024-12-14 17:02:37 -0800

    Improve `BinaryJaccardEvaluator`

--- src/arkas/evaluator/binary_jaccard.py
+++ src/arkas/evaluator/binary_jaccard.py
@@ -7,7 +7,10 @@ __all__ = ["BinaryJaccardEvaluator"]
 import logging
 from typing import TYPE_CHECKING
 
+from coola.utils.format import repr_mapping_line
+
 from arkas.evaluator.lazy import BaseLazyEvaluator
+from arkas.metric.utils import check_nan_policy
 from arkas.result import BinaryJaccardResult, Result
 from arkas.utils.array import to_array
 
@@ -40,7 +43,7 @@ class BinaryJaccardEvaluator(BaseLazyEvaluator[BinaryJaccardResult]):
     >>> from arkas.evaluator import BinaryJaccardEvaluator
     >>> evaluator = BinaryJaccardEvaluator(y_true="target", y_pred="pred")
     >>> evaluator
-    BinaryJaccardEvaluator(y_true=target, y_pred=pred, drop_nulls=True)
+    BinaryJaccardEvaluator(y_true='target', y_pred='pred', drop_nulls=True, nan_policy='propagate')
     >>> data = pl.DataFrame({"pred": [1, 0, 0, 1, 1], "target": [1, 0, 0, 1, 1]})
     >>> result = evaluator.evaluate(data)
     >>> result
@@ -49,27 +52,43 @@ class BinaryJaccardEvaluator(BaseLazyEvaluator[BinaryJaccardResult]):
     ```
     """
 
-    def __init__(self, y_true: str, y_pred: str, drop_nulls: bool = True) -> None:
+    def __init__(
+        self,
+        y_true: str,
+        y_pred: str,
+        drop_nulls: bool = True,
+        nan_policy: str = "propagate",
+    ) -> None:
         super().__init__(drop_nulls=drop_nulls)
         self._y_true = y_true
         self._y_pred = y_pred
 
+        check_nan_policy(nan_policy)
+        self._nan_policy = nan_policy
+
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}(y_true={self._y_true}, y_pred={self._y_pred}, "
-            f"drop_nulls={self._drop_nulls})"
+        args = repr_mapping_line(
+            {
+                "y_true": self._y_true,
+                "y_pred": self._y_pred,
+                "drop_nulls": self._drop_nulls,
+                "nan_policy": self._nan_policy,
+            }
         )
+        return f"{self.__class__.__qualname__}({args})"
 
     def evaluate(self, data: pl.DataFrame, lazy: bool = True) -> BinaryJaccardResult | Result:
         logger.info(
             f"Evaluating the binary Jaccard | y_true={self._y_true} | y_pred={self._y_pred} | "
-            f"drop_nulls={self._drop_nulls}"
+            f"drop_nulls={self._drop_nulls} | nan_policy={self._nan_policy}"
         )
         return self._evaluate(data, lazy)
 
     def _compute_result(self, data: pl.DataFrame) -> BinaryJaccardResult:
         return BinaryJaccardResult(
-            y_true=to_array(data[self._y_true]).ravel(), y_pred=to_array(data[self._y_pred]).ravel()
+            y_true=to_array(data[self._y_true]).ravel(),
+            y_pred=to_array(data[self._y_pred]).ravel(),
+            nan_policy=self._nan_policy,
         )
 
     def _get_columns(self) -> tuple[str, ...]:
