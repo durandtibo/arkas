b20285ea89a518f8489723e09d41d2053241b9f8
Thibaut Durand <durand.tibo@gmail.com>
2024-12-14 18:12:57 -0800

    Improve `MulticlassRocAucEvaluator`

--- tests/unit/evaluator/test_multiclass_roc_auc.py
+++ tests/unit/evaluator/test_multiclass_roc_auc.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 import polars as pl
+import pytest
 
 from arkas.evaluator import MulticlassRocAucEvaluator
 from arkas.result import EmptyResult, MulticlassRocAucResult, Result
@@ -223,5 +224,48 @@ def test_multiclass_roc_auc_evaluator_evaluate_drop_nulls_false() -> None:
                 ),
             ),
             equal_nan=True,
+        )
+    )
+
+
+@pytest.mark.parametrize("nan_policy", ["omit", "propagate", "raise"])
+def test_multiclass_roc_auc_evaluator_evaluate_nan_policy(nan_policy: str) -> None:
+    assert (
+        MulticlassRocAucEvaluator(y_true="target", y_score="pred", nan_policy=nan_policy)
+        .evaluate(
+            pl.DataFrame(
+                {
+                    "pred": [
+                        [0.7, 0.2, 0.1],
+                        [0.4, 0.3, 0.3],
+                        [0.1, 0.8, 0.1],
+                        [0.2, 0.5, 0.3],
+                        [0.3, 0.2, 0.5],
+                        [0.1, 0.2, 0.7],
+                        None,
+                        [0.1, 0.2, 0.7],
+                        None,
+                    ],
+                    "target": [0, 0, 1, 1, 2, 2, 0, None, None],
+                    "col": [1, None, 3, 4, 5, 6, None, 8, None],
+                },
+                schema={"pred": pl.Array(pl.Float64, 3), "target": pl.Int64, "col": pl.Int64},
+            )
+        )
+        .equal(
+            MulticlassRocAucResult(
+                y_true=np.array([0, 0, 1, 1, 2, 2]),
+                y_score=np.array(
+                    [
+                        [0.7, 0.2, 0.1],
+                        [0.4, 0.3, 0.3],
+                        [0.1, 0.8, 0.1],
+                        [0.2, 0.5, 0.3],
+                        [0.3, 0.2, 0.5],
+                        [0.1, 0.2, 0.7],
+                    ]
+                ),
+                nan_policy=nan_policy,
+            ),
         )
     )
