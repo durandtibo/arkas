bc9c45b3ecea3795735461a528850c90ce2a3369
Thibaut Durand <durand.tibo@gmail.com>
2024-12-29 12:08:13 -0800

    Add `BaseReporter` (#570)

--- src/arkas/reporter/__init__.py
+++ src/arkas/reporter/__init__.py
@@ -1,3 +1,7 @@
 r"""Contain reporters."""
 
 from __future__ import annotations
+
+__all__ = ["BaseReporter", "is_reporter_config", "setup_reporter"]
+
+from arkas.reporter.base import BaseReporter, is_reporter_config, setup_reporter
