9c09cfb59e04165074174cf256dc0b8aa6d1942a
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 13:53:21 -0800

    Add `BasePlotter` and `Plotter`

---
+++ src/arkas/plotter/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain data plotters."""
+
+from __future__ import annotations
+
+__all__ = ["BasePlotter", "Plotter"]
+
+from arkas.plotter.base import BasePlotter
+from arkas.plotter.vanilla import Plotter
