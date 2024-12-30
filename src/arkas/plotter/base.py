94fc86271cfb46a6f182b24104ff66baef758a67
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 13:56:52 -0800

    Add `BasePlotter` and `Plotter` (#582)

---
+++ src/arkas/plotter/base.py
@@ -0,0 +1,52 @@
+r"""Contain the base class to implement a plotter."""
+
+from __future__ import annotations
+
+__all__ = ["BasePlotter"]
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BasePlotter(ABC):
+    r"""Define the base class to implement a plotter.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arkas.plotter import Plotter
+    >>> plotter = Plotter()
+    >>> plotter
+    Plotter(count=0)
+
+    ```
+    """
+
+    @abstractmethod
+    def equal(self, other: Any, equal_nan: bool = False) -> bool:
+        r"""Indicate if two plotters are equal or not.
+
+        Args:
+            other: The other plotter to compare.
+            equal_nan: Whether to compare NaN's as equal. If ``True``,
+                NaN's in both objects will be considered equal.
+
+        Returns:
+            ``True`` if the two plotters are equal, otherwise ``False``.
+
+        Example usage:
+
+        ```pycon
+
+        >>> from arkas.plotter import Plotter
+        >>> plotter1 = Plotter()
+        >>> plotter2 = Plotter()
+        >>> plotter3 = Plotter({"fig": None})
+        >>> plotter1.equal(plotter2)
+        True
+        >>> plotter1.equal(plotter3)
+        False
+
+        ```
+        """
---
+++ tests/unit/plotter/test_vanilla.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+import pytest
+from coola import objects_are_equal
+
+from arkas.plotter import Plotter
+
+
+@pytest.fixture
+def figure() -> plt.Figure:
+    return plt.subplots()[0]
+
+
+#############################
+#     Tests for Plotter     #
+#############################
+
+
+def test_plotter_repr() -> None:
+    assert repr(Plotter()) == "Plotter(count=0)"
+
+
+def test_plotter_str() -> None:
+    assert str(Plotter()) == "Plotter(count=0)"
+
+
+def test_plotter_equal_true() -> None:
+    assert Plotter().equal(Plotter())
+
+
+def test_plotter_equal_false_different_figures(figure: plt.Figure) -> None:
+    assert not Plotter().equal(Plotter(figures={"fig": figure}))
+
+
+def test_plotter_equal_false_different_type() -> None:
+    assert not Plotter().equal(42)
+
+
+def test_plotter_equal_nan_true() -> None:
+    assert Plotter(figures={"accuracy": float("nan")}).equal(
+        Plotter(figures={"accuracy": float("nan")}),
+        equal_nan=True,
+    )
+
+
+def test_plotter_equal_nan_false() -> None:
+    assert not Plotter(figures={"accuracy": float("nan")}).equal(
+        Plotter(figures={"accuracy": float("nan")})
+    )
+
+
+def test_plotter_plot(figure: plt.Figure) -> None:
+    assert objects_are_equal(
+        Plotter(figures={"fig": figure}).plot(),
+        {"fig": figure},
+    )
+
+
+def test_plotter_plot_empty() -> None:
+    assert objects_are_equal(Plotter().plot(), {})
+
+
+def test_plotter_plot_prefix_suffix(figure: plt.Figure) -> None:
+    assert objects_are_equal(
+        Plotter(figures={"fig": figure}).plot(prefix="prefix_", suffix="_suffix"),
+        {"prefix_fig_suffix": figure},
+    )
