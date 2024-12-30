a0b51c5b374e014cf77faddc68e4f8426d54d9e7
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 14:05:26 -0800

    Add `BaseEvaluator` and `AccuracyEvaluator` (#583)

---
+++ src/arkas/evaluator2/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain data evaluators."""
+
+from __future__ import annotations
+
+__all__ = ["AccuracyEvaluator", "BaseEvaluator"]
+
+from arkas.evaluator2.accuracy import AccuracyEvaluator
+from arkas.evaluator2.base import BaseEvaluator
