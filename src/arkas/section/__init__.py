f3dc21c9be50ebc71c76652e077455b9ac828f9f
Thibaut Durand <durand.tibo@gmail.com>
2024-12-29 16:11:57 -0800

    Add `ResultSection`

--- src/arkas/section/__init__.py
+++ src/arkas/section/__init__.py
@@ -2,9 +2,16 @@ r"""Contain sections."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracySection", "BaseSection", "BinaryPrecisionSection", "ContentSection"]
+__all__ = [
+    "AccuracySection",
+    "BaseSection",
+    "BinaryPrecisionSection",
+    "ContentSection",
+    "ResultSection",
+]
 
 from arkas.section.accuracy import AccuracySection
 from arkas.section.base import BaseSection
 from arkas.section.binary_precision import BinaryPrecisionSection
 from arkas.section.content import ContentSection
+from arkas.section.result import ResultSection
