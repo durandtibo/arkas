28d8b89476d287be191035a045745ccccc132845
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 14:53:19 -0800

    Add `ContentGenerator` (#587)

--- src/arkas/hcg/__init__.py
+++ src/arkas/hcg/__init__.py
@@ -2,7 +2,8 @@ r"""Contain HTML Content Generators (HCGs)."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracyContentGenerator", "BaseContentGenerator"]
+__all__ = ["AccuracyContentGenerator", "BaseContentGenerator", "ContentGenerator"]
 
 from arkas.hcg.accuracy import AccuracyContentGenerator
 from arkas.hcg.base import BaseContentGenerator
+from arkas.hcg.vanilla import ContentGenerator
