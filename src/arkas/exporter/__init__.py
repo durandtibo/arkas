4c8287f23d2530b62b255c61fe19a2b0289083b3
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 07:28:59 -0800

    Add `FigureExporter`

--- src/arkas/exporter/__init__.py
+++ src/arkas/exporter/__init__.py
@@ -2,7 +2,14 @@ r"""Contain output exporters."""
 
 from __future__ import annotations
 
-__all__ = ["BaseExporter", "MetricExporter", "is_exporter_config", "setup_exporter"]
+__all__ = [
+    "BaseExporter",
+    "FigureExporter",
+    "MetricExporter",
+    "is_exporter_config",
+    "setup_exporter",
+]
 
 from arkas.exporter.base import BaseExporter, is_exporter_config, setup_exporter
+from arkas.exporter.figure import FigureExporter
 from arkas.exporter.metric import MetricExporter
