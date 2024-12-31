536fa314f5eab176d60977e546c2ded1e0d4cead
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 11:00:29 -0800

    Add `BalancedAccuracyOutput`

--- src/arkas/output/__init__.py
+++ src/arkas/output/__init__.py
@@ -2,9 +2,18 @@ r"""Contain data outputs."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracyOutput", "BaseLazyOutput", "BaseOutput", "EmptyOutput", "Output", "OutputDict"]
+__all__ = [
+    "AccuracyOutput",
+    "BalancedAccuracyOutput",
+    "BaseLazyOutput",
+    "BaseOutput",
+    "EmptyOutput",
+    "Output",
+    "OutputDict",
+]
 
 from arkas.output.accuracy import AccuracyOutput
+from arkas.output.balanced_accuracy import BalancedAccuracyOutput
 from arkas.output.base import BaseOutput
 from arkas.output.empty import EmptyOutput
 from arkas.output.lazy import BaseLazyOutput
---
+++ tests/unit/output/test_balanced_accuracy.py
@@ -0,0 +1,172 @@
+from __future__ import annotations
+
+import numpy as np
+import pytest
+
+from arkas.content import BalancedAccuracyContentGenerator
+from arkas.evaluator2 import BalancedAccuracyEvaluator, Evaluator
+from arkas.output import BalancedAccuracyOutput
+from arkas.plotter import Plotter
+from arkas.state import AccuracyState
+
+############################################
+#     Tests for BalancedAccuracyOutput     #
+############################################
+
+
+def test_balanced_accuracy_output_repr() -> None:
+    assert repr(
+        BalancedAccuracyOutput(
+            AccuracyState(
+                y_true=np.array([1, 0, 0, 1, 1]),
+                y_pred=np.array([1, 0, 0, 1, 1]),
+                y_true_name="target",
+                y_pred_name="pred",
+            ),
+        )
+    ).startswith("BalancedAccuracyOutput(")
+
+
+def test_balanced_accuracy_output_str() -> None:
+    assert str(
+        BalancedAccuracyOutput(
+            AccuracyState(
+                y_true=np.array([1, 0, 0, 1, 1]),
+                y_pred=np.array([1, 0, 0, 1, 1]),
+                y_true_name="target",
+                y_pred_name="pred",
+            ),
+        )
+    ).startswith("BalancedAccuracyOutput(")
+
+
+def test_balanced_accuracy_output_equal_true() -> None:
+    assert BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        ),
+    ).equal(
+        BalancedAccuracyOutput(
+            AccuracyState(
+                y_true=np.array([1, 0, 0, 1, 1]),
+                y_pred=np.array([1, 0, 0, 1, 1]),
+                y_true_name="target",
+                y_pred_name="pred",
+            ),
+        )
+    )
+
+
+def test_balanced_accuracy_output_equal_false_different_state() -> None:
+    assert not BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        ),
+    ).equal(
+        BalancedAccuracyOutput(
+            AccuracyState(
+                y_true=np.array([1, 0, 0, 1, 2]),
+                y_pred=np.array([1, 0, 0, 1, 1]),
+                y_true_name="target",
+                y_pred_name="pred",
+            ),
+        )
+    )
+
+
+def test_balanced_accuracy_output_equal_false_different_nan_policy() -> None:
+    assert not BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        ),
+    ).equal(
+        BalancedAccuracyOutput(
+            AccuracyState(
+                y_true=np.array([1, 0, 0, 1, 2]),
+                y_pred=np.array([1, 0, 0, 1, 1]),
+                y_true_name="target",
+                y_pred_name="pred",
+            ),
+            nan_policy="raise",
+        )
+    )
+
+
+def test_balanced_accuracy_output_equal_false_different_type() -> None:
+    assert not BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        ),
+    ).equal(42)
+
+
+@pytest.mark.parametrize("nan_policy", ["omit", "propagate", "raise"])
+def test_balanced_accuracy_output_get_content_generator(nan_policy: str) -> None:
+    state = AccuracyState(
+        y_true=np.array([1, 0, 0, 1, 1]),
+        y_pred=np.array([1, 0, 0, 1, 1]),
+        y_true_name="target",
+        y_pred_name="pred",
+    )
+    generator = BalancedAccuracyOutput(state, nan_policy).get_content_generator()
+    assert generator.equal(BalancedAccuracyContentGenerator(state, nan_policy))
+
+
+@pytest.mark.parametrize("nan_policy", ["omit", "propagate", "raise"])
+def test_balanced_accuracy_output_get_evaluator_lazy_true(nan_policy: str) -> None:
+    state = AccuracyState(
+        y_true=np.array([1, 0, 0, 1, 1]),
+        y_pred=np.array([1, 0, 0, 1, 1]),
+        y_true_name="target",
+        y_pred_name="pred",
+    )
+    evaluator = BalancedAccuracyOutput(state, nan_policy).get_evaluator()
+    assert evaluator.equal(BalancedAccuracyEvaluator(state, nan_policy))
+
+
+def test_balanced_accuracy_output_get_evaluator_lazy_false() -> None:
+    evaluator = BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        )
+    ).get_evaluator(lazy=False)
+    assert evaluator.equal(Evaluator({"balanced_accuracy": 1.0, "count": 5}))
+
+
+@pytest.mark.parametrize("nan_policy", ["omit", "propagate", "raise"])
+def test_balanced_accuracy_output_get_plotter_lazy_true(nan_policy: str) -> None:
+    state = AccuracyState(
+        y_true=np.array([1, 0, 0, 1, 1]),
+        y_pred=np.array([1, 0, 0, 1, 1]),
+        y_true_name="target",
+        y_pred_name="pred",
+    )
+    plotter = BalancedAccuracyOutput(state, nan_policy).get_plotter()
+    assert plotter.equal(Plotter())
+
+
+def test_balanced_accuracy_output_get_plotter_lazy_false() -> None:
+    plotter = BalancedAccuracyOutput(
+        AccuracyState(
+            y_true=np.array([1, 0, 0, 1, 1]),
+            y_pred=np.array([1, 0, 0, 1, 1]),
+            y_true_name="target",
+            y_pred_name="pred",
+        )
+    ).get_plotter(lazy=False)
+    assert plotter.equal(Plotter())
