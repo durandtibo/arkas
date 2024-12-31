42d743fc2a99a5463b25fa83123c407db197ccad
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 14:41:27 -0800

    Add `DataFrameSummaryContentGenerator` (#637)

--- src/arkas/content/__init__.py
+++ src/arkas/content/__init__.py
@@ -8,10 +8,12 @@ __all__ = [
     "BaseContentGenerator",
     "ContentGenerator",
     "ContentGeneratorDict",
+    "DataFrameSummaryContentGenerator",
 ]
 
 from arkas.content.accuracy import AccuracyContentGenerator
 from arkas.content.balanced_accuracy import BalancedAccuracyContentGenerator
 from arkas.content.base import BaseContentGenerator
+from arkas.content.frame_summary import DataFrameSummaryContentGenerator
 from arkas.content.mapping import ContentGeneratorDict
 from arkas.content.vanilla import ContentGenerator
