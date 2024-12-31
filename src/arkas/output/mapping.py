4a80e4843029680db21fa37df05ee6ee6b9a33a4
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 10:14:33 -0800

    Add `OutputDict`

--- src/arkas/output/mapping.py
+++ src/arkas/output/mapping.py
@@ -3,22 +3,27 @@ single output object."""
 
 from __future__ import annotations
 
-__all__ = ["MappingOutput"]
+__all__ = ["OutputDict"]
 
 from typing import TYPE_CHECKING, Any
 
 from coola import objects_are_equal
 from coola.utils import str_indent, str_mapping
 
-from arkas.output import BaseOutput
+from arkas.evaluator2.mapping import EvaluatorDict
+from arkas.hcg.mapping import ContentGeneratorDict
+from arkas.output.lazy import BaseLazyOutput
+from arkas.plotter.mapping import PlotterDict
 
 if TYPE_CHECKING:
     from collections.abc import Mapping
 
+    from arkas.output.base import BaseOutput
 
-class MappingOutput(BaseOutput):
-    r"""Implement an output that combines a mapping of output objects into
-    a single output object.
+
+class OutputDict(BaseLazyOutput):
+    r"""Implement an output that combines a mapping of output objects
+    into a single output object.
 
     Args:
         outputs: The mapping of output objects to combine.
@@ -28,17 +33,49 @@ class MappingOutput(BaseOutput):
     ```pycon
 
     >>> import numpy as np
-    >>> from arkas.output import MappingOutput, Output
-    >>> output = MappingOutput(
+    >>> from arkas.output import OutputDict, Output, AccuracyOutput
+    >>> from arkas.hcg import ContentGenerator
+    >>> from arkas.evaluator2 import Evaluator
+    >>> from arkas.plotter import Plotter
+    >>> from arkas.state import AccuracyState
+    >>> output = OutputDict(
     ...     {
-    ...         "class1": Output(metrics={"accuracy": 62.0, "count": 42}),
-    ...         "class2": Output(metrics={"accuracy": 42.0, "count": 42}),
+    ...         "one": Output(
+    ...             content=ContentGenerator("meow"), evaluator=Evaluator(), plotter=Plotter()
+    ...         ),
+    ...         "two": AccuracyOutput(
+    ...             AccuracyState(
+    ...                 y_true=np.array([1, 0, 0, 1, 1]),
+    ...                 y_pred=np.array([1, 0, 0, 1, 1]),
+    ...                 y_true_name="target",
+    ...                 y_pred_name="pred",
+    ...             )
+    ...         ),
     ...     }
     ... )
     >>> output
-    MappingOutput(count=2)
-    >>> output.compute_metrics()
-    {'class1': {'accuracy': 62.0, 'count': 42}, 'class2': {'accuracy': 42.0, 'count': 42}}
+    OutputDict(count=2)
+    >>> output.get_content_generator()
+    ContentGeneratorDict(
+      (one): ContentGenerator()
+      (two): AccuracyContentGenerator(
+          (state): AccuracyState(y_true=(5,), y_pred=(5,), y_true_name='target', y_pred_name='pred')
+          (nan_policy): propagate
+        )
+    )
+    >>> output.get_evaluator()
+    EvaluatorDict(
+      (one): Evaluator(count=0)
+      (two): AccuracyEvaluator(
+          (state): AccuracyState(y_true=(5,), y_pred=(5,), y_true_name='target', y_pred_name='pred')
+          (nan_policy): propagate
+        )
+    )
+    >>> output.get_plotter()
+    PlotterDict(
+      (one): Plotter(count=0)
+      (two): Plotter(count=0)
+    )
 
     ```
     """
@@ -58,3 +95,13 @@ class MappingOutput(BaseOutput):
             return False
         return objects_are_equal(self._outputs, other._outputs, equal_nan=equal_nan)
 
+    def get_content_generator(self) -> ContentGeneratorDict:
+        return ContentGeneratorDict(
+            {key: output.get_content_generator() for key, output in self._outputs.items()}
+        )
+
+    def _get_evaluator(self) -> EvaluatorDict:
+        return EvaluatorDict({key: output.get_evaluator() for key, output in self._outputs.items()})
+
+    def _get_plotter(self) -> PlotterDict:
+        return PlotterDict({key: output.get_plotter() for key, output in self._outputs.items()})
