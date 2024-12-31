41973204e578aa65b2ea41a7efffe49ad848cc94
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 19:07:59 -0800

    Add `BaseAnalyzer`

---
+++ src/arkas/analyzer/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain DataFrame analyzers."""
+
+from __future__ import annotations
+
+__all__ = ["BaseAnalyzer", "is_analyzer_config", "setup_analyzer"]
+
+from arkas.analyzer.base import BaseAnalyzer, is_analyzer_config, setup_analyzer
