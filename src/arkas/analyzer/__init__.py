afb95fb7deaa61874adbcc010d08f98a5545359a
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 15:15:29 -0800

    Add `DataFrameSummaryAnalyzer` (#638)

--- src/arkas/analyzer/__init__.py
+++ src/arkas/analyzer/__init__.py
@@ -7,6 +7,7 @@ __all__ = [
     "BalancedAccuracyAnalyzer",
     "BaseAnalyzer",
     "BaseTruePredAnalyzer",
+    "DataFrameSummaryAnalyzer",
     "MappingAnalyzer",
     "TransformAnalyzer",
     "is_analyzer_config",
@@ -17,5 +18,6 @@ from arkas.analyzer.accuracy import AccuracyAnalyzer
 from arkas.analyzer.balanced_accuracy import BalancedAccuracyAnalyzer
 from arkas.analyzer.base import BaseAnalyzer, is_analyzer_config, setup_analyzer
 from arkas.analyzer.columns import BaseTruePredAnalyzer
+from arkas.analyzer.frame_summary import DataFrameSummaryAnalyzer
 from arkas.analyzer.mapping import MappingAnalyzer
 from arkas.analyzer.transform import TransformAnalyzer
