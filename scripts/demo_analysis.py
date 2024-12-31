42a3d957b4ac58417112ef642a2a87f5ea6b7483
Thibaut Durand <durand.tibo@gmail.com>
2024-12-31 01:01:54 -0800

    Add demo with `AnalysisRunner` (#607)

---
+++ scripts/demo_analysis.py
@@ -0,0 +1,49 @@
+# noqa: INP001
+r"""Contain a demo example."""
+
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+import numpy as np
+import polars as pl
+from grizz.ingestor import Ingestor
+from grizz.transformer import SequentialTransformer
+
+from arkas.analyzer import AccuracyAnalyzer
+from arkas.exporter import MetricExporter
+from arkas.runner import AnalysisRunner
+from arkas.utils.logging import configure_logging
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    r"""Define the main function."""
+    n_samples = 1000
+    rng = np.random.default_rng(42)
+    ingestor = Ingestor(
+        pl.DataFrame(
+            {
+                "pred": rng.integers(0, 2, (n_samples,)),
+                "score": rng.normal(0, 1, (n_samples,)),
+                "target": rng.integers(0, 2, (n_samples,)),
+            }
+        )
+    )
+
+    metric_path = Path.cwd().joinpath("tmp/metrics.pkl")
+    runner = AnalysisRunner(
+        ingestor=ingestor,
+        transformer=SequentialTransformer(transformers=[]),
+        analyzer=AccuracyAnalyzer(y_true="target", y_pred="pred"),
+        exporter=MetricExporter(path=metric_path),
+    )
+    logger.info(f"runner:\n{runner}")
+    runner.run()
+
+
+if __name__ == "__main__":
+    configure_logging(level=logging.INFO)
+    main()
