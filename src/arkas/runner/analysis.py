dad514f75020568dc22a355eb38e3b6da8554322
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 23:58:38 -0800

    Add `AnalysisRunner`

--- src/arkas/runner/analysis.py
+++ src/arkas/runner/analysis.py
@@ -2,7 +2,7 @@ r"""Contain a simple evaluation runner implementation."""
 
 from __future__ import annotations
 
-__all__ = ["EvaluationRunner"]
+__all__ = ["AnalysisRunner"]
 
 import logging
 from typing import TYPE_CHECKING, Any
@@ -14,24 +14,27 @@ from grizz.ingestor import BaseIngestor, setup_ingestor
 from grizz.transformer import BaseTransformer, setup_transformer
 from iden.io import BaseSaver, setup_saver
 
-from arkas.evaluator import BaseEvaluator, setup_evaluator
+from arkas.analyzer.base import BaseAnalyzer, setup_analyzer
+from arkas.reporter import BaseReporter, setup_reporter
 from arkas.runner.base import BaseRunner
 
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from arkas.output import BaseOutput
+
 logger = logging.getLogger(__name__)
 
 
-class EvaluationRunner(BaseRunner):
-    r"""Implement a simple evaluation runner.
+class AnalysisRunner(BaseRunner):
+    r"""Implement a runner to analyze data.
 
     Args:
         ingestor: The data ingestor or its configuration.
         transformer: The data transformer or its configuration.
-        evaluator: The evaluator or its configuration.
-        saver: The metric saver or its configuration.
-        path: The path where to save the metrics.
+        analyzer: The analyzer or its configuration.
+        metric_saver: The metric saver or its configuration.
+        metric_path: The path where to save the metrics.
         show_metrics: If ``True``, the metrics are shown in the
             logging output.
 
@@ -45,11 +48,12 @@ class EvaluationRunner(BaseRunner):
     >>> from iden.io import PickleSaver
     >>> from grizz.ingestor import Ingestor
     >>> from grizz.transformer import SequentialTransformer
-    >>> from arkas.evaluator import AccuracyEvaluator
-    >>> from arkas.runner import EvaluationRunner
+    >>> from arkas.analyzer import AccuracyAnalyzer
+    >>> from arkas.reporter import Reporter
+    >>> from arkas.runner import AnalysisRunner
     >>> with tempfile.TemporaryDirectory() as tmpdir:
     ...     path = Path(tmpdir).joinpath("metrics.pkl")
-    ...     runner = EvaluationRunner(
+    ...     runner = AnalysisRunner(
     ...         ingestor=Ingestor(
     ...             pl.DataFrame(
     ...                 {
@@ -59,7 +63,8 @@ class EvaluationRunner(BaseRunner):
     ...             )
     ...         ),
     ...         transformer=SequentialTransformer(transformers=[]),
-    ...         evaluator=AccuracyEvaluator(y_true="target", y_pred="pred"),
+    ...         analyzer=AccuracyAnalyzer(y_true="target", y_pred="pred"),
+    ...         reporter=Reporter(),
     ...         saver=PickleSaver(),
     ...         path=path,
     ...     )
@@ -69,7 +74,7 @@ class EvaluationRunner(BaseRunner):
     EvaluationRunner(
       (ingestor): Ingestor(shape=(5, 2))
       (transformer): SequentialTransformer()
-      (evaluator): AccuracyEvaluator(y_true='target', y_pred='pred', drop_nulls=True, nan_policy='propagate')
+      (analyzer): AccuracyEvaluator(y_true='target', y_pred='pred', drop_nulls=True, nan_policy='propagate')
       (saver): PickleSaver(protocol=5)
       (path): .../metrics.pkl
       (show_metrics): True
@@ -82,16 +87,18 @@ class EvaluationRunner(BaseRunner):
         self,
         ingestor: BaseIngestor | dict,
         transformer: BaseTransformer | dict,
-        evaluator: BaseEvaluator | dict,
-        saver: BaseSaver | dict,
-        path: Path | str,
+        analyzer: BaseAnalyzer | dict,
+        reporter: BaseReporter | dict,
+        metric_saver: BaseSaver | dict,
+        metric_path: Path | str,
         show_metrics: bool = True,
     ) -> None:
         self._ingestor = setup_ingestor(ingestor)
         self._transformer = setup_transformer(transformer)
-        self._evaluator = setup_evaluator(evaluator)
-        self._saver = setup_saver(saver)
-        self._path = sanitize_path(path)
+        self._analyzer = setup_analyzer(analyzer)
+        self._reporter = setup_reporter(reporter)
+        self._metric_saver = setup_saver(metric_saver)
+        self._metric_path = sanitize_path(metric_path)
         self._show_metrics = bool(show_metrics)
 
     def __repr__(self) -> str:
@@ -100,9 +107,10 @@ class EvaluationRunner(BaseRunner):
                 {
                     "ingestor": self._ingestor,
                     "transformer": self._transformer,
-                    "evaluator": self._evaluator,
-                    "saver": self._saver,
-                    "path": self._path,
+                    "analyzer": self._analyzer,
+                    "reporter": self._reporter,
+                    "metric_saver": self._metric_saver,
+                    "metric_path": self._metric_path,
                     "show_metrics": self._show_metrics,
                 }
             )
@@ -114,13 +122,19 @@ class EvaluationRunner(BaseRunner):
         raw_data = self._ingestor.ingest()
         logger.info("Transforming data...")
         data = self._transformer.transform(raw_data)
-        logger.info("Evaluating...")
-        result = self._evaluator.evaluate(data)
-        logger.info(f"result:\n{result}")
+        logger.info("Analyzing...")
+        output = self._analyzer.analyze(data)
+        logger.info(f"output:\n{output}")
+
+        self._export_metrics(output)
+
+        self._reporter.generate(output)
+
+    def _export_metrics(self, output: BaseOutput) -> None:
         logger.info("Computing metrics...")
-        metrics = result.compute_metrics()
-        logger.info(f"Saving metrics at {self._path}...")
-        self._saver.save(metrics, path=self._path, exist_ok=True)
+        metrics = output.get_evaluator().evaluate()
+        logger.info(f"Saving metrics at {self._metric_path}...")
+        self._metric_saver.save(metrics, path=self._metric_path, exist_ok=True)
 
         if self._show_metrics:
             logger.info(f"metrics:\n{str_mapping(to_flat_dict(metrics), sorted_keys=True)}")
