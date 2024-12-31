a86120b727e5e641badcea5b65d6124e42785640
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 16:06:25 -0800

    Add `Reporter`

--- src/arkas/reporter/__init__.py
+++ src/arkas/reporter/__init__.py
@@ -2,7 +2,8 @@ r"""Contain reporters."""
 
 from __future__ import annotations
 
-__all__ = ["BaseReporter", "EvalReporter", "is_reporter_config", "setup_reporter"]
+__all__ = ["BaseReporter", "EvalReporter", "Reporter", "is_reporter_config", "setup_reporter"]
 
 from arkas.reporter.base import BaseReporter, is_reporter_config, setup_reporter
 from arkas.reporter.eval import EvalReporter
+from arkas.reporter.vanilla import Reporter
