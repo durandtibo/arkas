195db3777462436e6cc7fb6f9db640cf77df4e64
Thibaut Durand <durand.tibo@gmail.com>
2024-12-30 19:47:37 -0800

    Add `AccuracyAnalyzer`

--- src/arkas/analyzer/__init__.py
+++ src/arkas/analyzer/__init__.py
@@ -2,6 +2,14 @@ r"""Contain DataFrame analyzers."""
 
 from __future__ import annotations
 
-__all__ = ["BaseAnalyzer", "is_analyzer_config", "setup_analyzer"]
+__all__ = [
+    "AccuracyAnalyzer",
+    "BaseAnalyzer",
+    "BaseTruePredAnalyzer",
+    "is_analyzer_config",
+    "setup_analyzer",
+]
 
+from arkas.analyzer.accuracy import AccuracyAnalyzer
 from arkas.analyzer.base import BaseAnalyzer, is_analyzer_config, setup_analyzer
+from arkas.analyzer.columns import BaseTruePredAnalyzer
