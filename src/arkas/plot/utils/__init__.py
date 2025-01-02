ea81f356c9c04f58b9d056ae0ae500250f021168
Thibaut Durand <durand.tibo@gmail.com>
2025-01-02 13:24:41 -0800

    Add `readable_yticklabels`

--- src/arkas/plot/utils/__init__.py
+++ src/arkas/plot/utils/__init__.py
@@ -8,8 +8,9 @@ __all__ = [
     "axvline_median",
     "axvline_quantile",
     "readable_xticklabels",
+    "readable_yticklabels",
 ]
 
 from arkas.plot.utils.line import axvline_median, axvline_quantile
 from arkas.plot.utils.scale import auto_yscale_continuous, auto_yscale_discrete
-from arkas.plot.utils.tick import readable_xticklabels
+from arkas.plot.utils.tick import readable_xticklabels, readable_yticklabels
