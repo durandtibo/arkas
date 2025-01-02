5adb6a76181162f4cfe334736451611a27a31855
Thibaut Durand <durand.tibo@gmail.com>
2025-01-02 00:35:51 -0800

    Add `PrecisionRecallState`

--- src/arkas/state/__init__.py
+++ src/arkas/state/__init__.py
@@ -2,7 +2,8 @@ r"""Contain states."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracyState", "BaseState"]
+__all__ = ["AccuracyState", "BaseState", "PrecisionRecallState"]
 
 from arkas.state.accuracy import AccuracyState
 from arkas.state.base import BaseState
+from arkas.state.precision_recall import PrecisionRecallState
