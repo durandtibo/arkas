2a8d56a1e6cb9b23fe4bab19a148b2dbabdaed10
Thibaut Durand <durand.tibo@gmail.com>
2025-01-02 08:18:34 -0800

    Add `ColumnCooccurrenceContentGenerator`

--- src/arkas/content/__init__.py
+++ src/arkas/content/__init__.py
@@ -1,4 +1,4 @@
-r"""Contain HTML Content Generators (HCGs)."""
+r"""Contain HTML content generators."""
 
 from __future__ import annotations
 
@@ -6,6 +6,7 @@ __all__ = [
     "AccuracyContentGenerator",
     "BalancedAccuracyContentGenerator",
     "BaseContentGenerator",
+    "ColumnCooccurrenceContentGenerator",
     "ContentGenerator",
     "ContentGeneratorDict",
     "DataFrameSummaryContentGenerator",
@@ -14,6 +15,7 @@ __all__ = [
 from arkas.content.accuracy import AccuracyContentGenerator
 from arkas.content.balanced_accuracy import BalancedAccuracyContentGenerator
 from arkas.content.base import BaseContentGenerator
+from arkas.content.column_cooccurrence import ColumnCooccurrenceContentGenerator
 from arkas.content.frame_summary import DataFrameSummaryContentGenerator
 from arkas.content.mapping import ContentGeneratorDict
 from arkas.content.vanilla import ContentGenerator
---
+++ src/arkas/content/column_cooccurrence.py
@@ -0,0 +1,109 @@
+r"""Contain the implementation of a HTML content generator that returns
+the pairwise column co-occurrence."""
+
+from __future__ import annotations
+
+__all__ = [
+    "ColumnCooccurrenceContentGenerator",
+    "create_template",
+]
+
+import logging
+from typing import TYPE_CHECKING, Any
+
+from coola import objects_are_equal
+from jinja2 import Template
+
+from arkas.content.section import BaseSectionContentGenerator
+
+if TYPE_CHECKING:
+
+    import polars as pl
+
+logger = logging.getLogger(__name__)
+
+
+class ColumnCooccurrenceContentGenerator(BaseSectionContentGenerator):
+    r"""Implement a content generator that returns pairwise column co-
+    occurrence.
+
+    Args:
+        frame: The DataFrame to analyze.
+        ignore_self: If ``True``, the diagonal of the co-occurrence
+            matrix (a.k.a. self-co-occurrence) is set to 0.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import polars as pl
+    >>> from arkas.content import ColumnCooccurrenceContentGenerator
+    >>> content = ColumnCooccurrenceContentGenerator(
+    ...     frame=pl.DataFrame(
+    ...         {
+    ...             "col1": [1.2, 4.2, 4.2, 2.2],
+    ...             "col2": [1, 1, 1, 1],
+    ...             "col3": [1, 2, 2, 2],
+    ...         },
+    ...         schema={"col1": pl.Float64, "col2": pl.Int64, "col3": pl.Int64},
+    ...     )
+    ... )
+    >>> content
+    ColumnCooccurrenceContentGenerator(shape=(4, 3), ignore_self=False)
+
+    ```
+    """
+
+    def __init__(self, frame: pl.DataFrame, ignore_self: bool = False) -> None:
+        self._frame = frame
+        self._ignore_self = bool(ignore_self)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(shape={self._frame.shape}, ignore_self={self._ignore_self})"
+
+    @property
+    def frame(self) -> pl.DataFrame:
+        r"""The DataFrame to analyze."""
+        return self._frame
+
+    @property
+    def ignore_self(self) -> bool:
+        return self._ignore_self
+
+    def equal(self, other: Any, equal_nan: bool = False) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.ignore_self == other.ignore_self and objects_are_equal(
+            self.frame, other.frame, equal_nan=equal_nan
+        )
+
+    def generate_content(self) -> str:
+        logger.info("Generating the DataFrame summary content...")
+        return Template(create_template()).render(
+            {
+                "nrows": f"{self._frame.shape[0]:,}",
+                "ncols": f"{self._frame.shape[1]:,}",
+            }
+        )
+
+
+def create_template() -> str:
+    r"""Return the template of the content.
+
+    Returns:
+        The content template.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arkas.content.frame_summary import create_template
+    >>> template = create_template()
+
+    ```
+    """
+    return """<ul>
+  <li> number of rows: {{nrows}}</li>
+  <li> number of columns: {{ncols}} </li>
+</ul>
+"""
