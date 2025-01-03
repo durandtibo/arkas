08729183e21ad54b4127392109899248f28f7cc7
Thibaut Durand <durand.tibo@gmail.com>
2025-01-02 23:41:30 -0800

    Add `is_matplotlib_available` (#697)

--- src/arkas/testing.py
+++ src/arkas/testing.py
@@ -7,6 +7,7 @@ __all__ = [
     "hya_available",
     "hydra_available",
     "markdown_available",
+    "matplotlib_available",
     "omegaconf_available",
     "plotly_available",
     "scipy_available",
@@ -19,6 +20,7 @@ from arkas.utils.imports import (
     is_hya_available,
     is_hydra_available,
     is_markdown_available,
+    is_matplotlib_available,
     is_omegaconf_available,
     is_plotly_available,
     is_scipy_available,
@@ -28,6 +30,9 @@ colorlog_available = pytest.mark.skipif(not is_colorlog_available(), reason="req
 hya_available = pytest.mark.skipif(not is_hya_available(), reason="requires hya")
 hydra_available = pytest.mark.skipif(not is_hydra_available(), reason="requires hydra")
 markdown_available = pytest.mark.skipif(not is_markdown_available(), reason="requires markdown")
+matplotlib_available = pytest.mark.skipif(
+    not is_matplotlib_available(), reason="requires matplotlib"
+)
 omegaconf_available = pytest.mark.skipif(not is_omegaconf_available(), reason="requires omegaconf")
 plotly_available = pytest.mark.skipif(not is_plotly_available(), reason="requires plotly")
 scipy_available = pytest.mark.skipif(not is_scipy_available(), reason="requires scipy")
