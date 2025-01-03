16a342f29dfbf425bcb1560cfa442485383b260f
Thibaut Durand <durand.tibo@gmail.com>
2025-01-02 17:09:19 -0800

    Add `DefaultFigureConfig` (#682)

--- src/arkas/figure/__init__.py
+++ src/arkas/figure/__init__.py
@@ -5,11 +5,13 @@ from __future__ import annotations
 __all__ = [
     "BaseFigure",
     "BaseFigureConfig",
+    "DefaultFigureConfig",
     "HtmlFigure",
     "MatplotlibFigure",
     "MatplotlibFigureConfig",
 ]
 
 from arkas.figure.base import BaseFigure, BaseFigureConfig
+from arkas.figure.default import DefaultFigureConfig
 from arkas.figure.html import HtmlFigure
 from arkas.figure.matplotlib import MatplotlibFigure, MatplotlibFigureConfig
---
+++ tests/unit/figure/test_default.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+
+from arkas.figure import DefaultFigureConfig
+
+#########################################
+#     Tests for DefaultFigureConfig     #
+#########################################
+
+
+def test_default_figure_config_backend() -> None:
+    assert DefaultFigureConfig.backend() == "default"
+
+
+def test_default_figure_config_repr() -> None:
+    assert repr(DefaultFigureConfig()) == "DefaultFigureConfig()"
+
+
+def test_default_figure_config_str() -> None:
+    assert str(DefaultFigureConfig()) == "DefaultFigureConfig()"
+
+
+def test_default_figure_config_equal_true() -> None:
+    assert DefaultFigureConfig().equal(DefaultFigureConfig())
+
+
+def test_default_figure_config_equal_false_different_type() -> None:
+    assert not DefaultFigureConfig().equal(42)
+
+
+def test_default_figure_config_get_args() -> None:
+    assert DefaultFigureConfig().get_args() == {}
