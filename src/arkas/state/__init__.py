6042af0a48fabfb7fef4f653c27c34bd5e14ece6
Thibaut Durand <durand.tibo@gmail.com>
2025-01-04 14:54:23 -0800

    Add `ColumnCooccurrenceState`

--- src/arkas/state/__init__.py
+++ src/arkas/state/__init__.py
@@ -2,9 +2,16 @@ r"""Contain states."""
 
 from __future__ import annotations
 
-__all__ = ["AccuracyState", "BaseState", "DataFrameState", "PrecisionRecallState"]
+__all__ = [
+    "AccuracyState",
+    "BaseState",
+    "ColumnCooccurrenceState",
+    "DataFrameState",
+    "PrecisionRecallState",
+]
 
 from arkas.state.accuracy import AccuracyState
 from arkas.state.base import BaseState
+from arkas.state.column_cooccurrence import ColumnCooccurrenceState
 from arkas.state.dataframe import DataFrameState
 from arkas.state.precision_recall import PrecisionRecallState
