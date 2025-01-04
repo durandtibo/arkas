1b154b251e4add30a2d88ace36b273694bff9dde
Thibaut Durand <durand.tibo@gmail.com>
2025-01-03 16:46:11 -0800

    Add `get_default_config`

--- src/arkas/figure/utils.py
+++ src/arkas/figure/utils.py
@@ -2,12 +2,14 @@ r"""Contain utility functions to manage figures."""
 
 from __future__ import annotations
 
-__all__ = ["MISSING_FIGURE_MESSAGE", "figure2html"]
+__all__ = ["MISSING_FIGURE_MESSAGE", "figure2html", "get_default_config"]
 
 from typing import TYPE_CHECKING
 
+from arkas.figure.matplotlib import MatplotlibFigureConfig
+
 if TYPE_CHECKING:
-    from arkas.figure import BaseFigure
+    from arkas.figure.base import BaseFigure, BaseFigureConfig
 
 MISSING_FIGURE_MESSAGE = (
     "<span>&#9888;</span> No figure is generated because of missing or incorrect data"
@@ -42,3 +44,23 @@ def figure2html(figure: BaseFigure, reactive: bool = True, close_fig: bool = Fal
     if close_fig:
         figure.close()
     return data
+
+
+def get_default_config() -> BaseFigureConfig:
+    r"""Get the default figure config.
+
+    Returns:
+        The default figure config.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arkas.figure import get_default_config
+    >>> config = get_default_config()
+    >>> config
+    MatplotlibFigureConfig(color_norm=None)
+
+    ```
+    """
+    return MatplotlibFigureConfig()
