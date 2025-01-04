2836b2150d90ebcaf3c537b128d4119030555ae2
Thibaut Durand <durand.tibo@gmail.com>
2025-01-04 14:28:43 -0800

    Add `check_square_matrix` (#724)

--- src/arkas/utils/array.py
+++ src/arkas/utils/array.py
@@ -2,13 +2,41 @@ r"""Implement some utility functions for ``numpy.ndarray``s."""
 
 from __future__ import annotations
 
-__all__ = ["filter_range", "nonnan", "rand_replace", "to_array"]
+__all__ = ["check_square_matrix", "filter_range", "nonnan", "rand_replace", "to_array"]
 
 from typing import Any
 
 import numpy as np
 import polars as pl
 from coola.utils.array import to_array as coola_to_array
+
+
+def check_square_matrix(name: str, array: np.ndarray) -> None:
+    r"""Check if the input array is a square matrix.
+
+    Args:
+        name: The name of the variable.
+        array: The array to check.
+
+    Raises:
+        ValueError: if the array is not a square matrix.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from arkas.utils.array import check_square_matrix
+    >>> check_square_matrix("var", np.ones((3, 3)))
+
+    ```
+    """
+    if array.ndim != 2 or array.shape[0] != array.shape[1]:
+        msg = (
+            f"Incorrect {name!r}. The array must be a square matrix but received an array of "
+            f"shape {array.shape}"
+        )
+        raise ValueError(msg)
 
 
 def filter_range(array: np.ndarray, xmin: float, xmax: float) -> np.ndarray:
--- tests/unit/utils/test_array.py
+++ tests/unit/utils/test_array.py
@@ -7,7 +7,37 @@ import polars as pl
 import pytest
 from coola import objects_are_equal
 
-from arkas.utils.array import filter_range, nonnan, rand_replace, to_array
+from arkas.utils.array import (
+    check_square_matrix,
+    filter_range,
+    nonnan,
+    rand_replace,
+    to_array,
+)
+
+#########################################
+#     Tests for check_square_matrix     #
+#########################################
+
+
+def test_check_square_matrix_correct() -> None:
+    check_square_matrix("my_var", np.ones((3, 3)))
+
+
+def test_check_square_matrix_1d() -> None:
+    with pytest.raises(ValueError, match="Incorrect 'my_var'"):
+        check_square_matrix("my_var", np.ones((3,)))
+
+
+def test_check_square_matrix_3d() -> None:
+    with pytest.raises(ValueError, match="Incorrect 'my_var'"):
+        check_square_matrix("my_var", np.ones((3, 3, 3)))
+
+
+def test_check_square_matrix_not_square() -> None:
+    with pytest.raises(ValueError, match="Incorrect 'my_var'"):
+        check_square_matrix("my_var", np.ones((3, 4)))
+
 
 ##################################
 #     Tests for filter_range     #
