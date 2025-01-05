6247610243b2efd70591c449fa4e16422ee1edbb
Thibaut Durand <durand.tibo@gmail.com>
2025-01-05 10:43:28 -0800

    Add `ScatterDataFrameState`

--- src/arkas/state/__init__.py
+++ src/arkas/state/__init__.py
@@ -8,6 +8,7 @@ __all__ = [
     "ColumnCooccurrenceState",
     "DataFrameState",
     "PrecisionRecallState",
+    "ScatterDataFrameState",
     "TemporalDataFrameState",
 ]
 
@@ -16,4 +17,5 @@ from arkas.state.base import BaseState
 from arkas.state.column_cooccurrence import ColumnCooccurrenceState
 from arkas.state.dataframe import DataFrameState
 from arkas.state.precision_recall import PrecisionRecallState
+from arkas.state.scatter_dataframe import ScatterDataFrameState
 from arkas.state.temporal_dataframe import TemporalDataFrameState
