e5d24f782876c9822f9164a8ff6fe3c26064d684
Thibaut Durand <durand.tibo@gmail.com>
2025-01-04 22:52:38 -0800

    Add `TemporalPlotColumnPlotter` (#736)

--- src/arkas/plotter/__init__.py
+++ src/arkas/plotter/__init__.py
@@ -8,10 +8,12 @@ __all__ = [
     "PlotColumnPlotter",
     "Plotter",
     "PlotterDict",
+    "TemporalPlotColumnPlotter",
 ]
 
 from arkas.plotter.base import BasePlotter
 from arkas.plotter.column_cooccurrence import ColumnCooccurrencePlotter
 from arkas.plotter.mapping import PlotterDict
 from arkas.plotter.plot_column import PlotColumnPlotter
+from arkas.plotter.temporal_plot_column import TemporalPlotColumnPlotter
 from arkas.plotter.vanilla import Plotter
