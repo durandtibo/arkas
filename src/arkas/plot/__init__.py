d865ef9ed96d93657b1b8c29813dbc720e12b2dc
Thibaut Durand <durand.tibo@gmail.com>
2025-01-05 22:29:32 -0800

    Add `plot_null_temporal`

--- src/arkas/plot/__init__.py
+++ src/arkas/plot/__init__.py
@@ -2,7 +2,8 @@ r"""Contain plotting functionalities."""
 
 from __future__ import annotations
 
-__all__ = ["binary_precision_recall_curve", "binary_roc_curve"]
+__all__ = ["binary_precision_recall_curve", "binary_roc_curve", "plot_null_temporal"]
 
+from arkas.plot.null_temporal import plot_null_temporal
 from arkas.plot.pr import binary_precision_recall_curve
 from arkas.plot.roc import binary_roc_curve
