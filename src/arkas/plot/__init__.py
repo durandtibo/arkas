69e678f1731ca09b41288e3c096b78d72f744183
Thibaut Durand <durand.tibo@gmail.com>
2025-01-05 23:47:16 -0800

    Add `plot_cdf`

--- src/arkas/plot/__init__.py
+++ src/arkas/plot/__init__.py
@@ -2,8 +2,9 @@ r"""Contain plotting functionalities."""
 
 from __future__ import annotations
 
-__all__ = ["binary_precision_recall_curve", "binary_roc_curve", "plot_null_temporal"]
+__all__ = ["binary_precision_recall_curve", "binary_roc_curve", "plot_cdf", "plot_null_temporal"]
 
+from arkas.plot.cdf import plot_cdf
 from arkas.plot.null_temporal import plot_null_temporal
 from arkas.plot.pr import binary_precision_recall_curve
 from arkas.plot.roc import binary_roc_curve
