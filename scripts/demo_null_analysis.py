41112ea86f1d3a285be5ba2e6246903ce8c6527f
Thibaut Durand <durand.tibo@gmail.com>
2025-01-05 22:14:48 -0800

    Add demo script for null value analysis (#761)

---
+++ scripts/demo_null_analysis.py
@@ -0,0 +1,90 @@
+# noqa: INP001
+r"""Contain a demo example."""
+
+from __future__ import annotations
+
+import logging
+from datetime import datetime, timezone
+from pathlib import Path
+
+import numpy as np
+import polars as pl
+from grizz.ingestor import Ingestor
+from grizz.transformer import SequentialTransformer
+from grizz.utils.datetime import find_end_datetime
+
+from arkas import analyzer as aa
+from arkas.exporter import (
+    FigureExporter,
+    MetricExporter,
+    ReportExporter,
+    SequentialExporter,
+)
+from arkas.figure import MatplotlibFigureConfig
+from arkas.runner import AnalysisRunner
+from arkas.utils.array import rand_replace
+from arkas.utils.logging import configure_logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_dataframe() -> pl.DataFrame:
+    r"""Return a DataFrame."""
+    n_samples = 1000
+    ncols = 10
+    rng = np.random.default_rng(42)
+    frame = pl.DataFrame(
+        {
+            f"col{i}": rand_replace(
+                rng.normal(size=(n_samples,)),
+                value=None,
+                prob=i / ncols,
+                rng=rng,
+            ).tolist()
+            for i in range(ncols)
+        }
+    )
+    return frame.with_columns(
+        pl.datetime_range(
+            start=datetime(year=2018, month=1, day=1, tzinfo=timezone.utc),
+            end=find_end_datetime(
+                datetime(year=2018, month=1, day=1, tzinfo=timezone.utc),
+                periods=n_samples - 1,
+                interval="1h",
+            ),
+            interval="1h",
+        ).alias("datetime"),
+    )
+
+
+def main() -> None:
+    r"""Define the main function."""
+    ingestor = Ingestor(get_dataframe())
+
+    path = Path.cwd().joinpath("tmp")
+    figure_config = MatplotlibFigureConfig(init={"dpi": 500, "figsize": (14, 6)})
+    runner = AnalysisRunner(
+        ingestor=ingestor,
+        transformer=SequentialTransformer(transformers=[]),
+        analyzer=aa.MappingAnalyzer(
+            {
+                "summary": aa.DataFrameSummaryAnalyzer(),
+                "plot columns": aa.NullValueAnalyzer(figure_config=figure_config),
+            }
+        ),
+        exporter=SequentialExporter(
+            [
+                FigureExporter(path=path.joinpath("figures.pkl"), exist_ok=True),
+                ReportExporter(path=path.joinpath("report.html"), exist_ok=True),
+                MetricExporter(path=path.joinpath("metrics.pkl"), exist_ok=True),
+            ]
+        ),
+        lazy=False,
+    )
+    logger.info(f"runner:\n{runner}")
+    runner.run()
+
+
+if __name__ == "__main__":
+    configure_logging(level=logging.INFO)
+    main()
