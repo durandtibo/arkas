2efd80f708c54e3033b4beec7420566ef40cc95f
Thibaut Durand <durand.tibo@gmail.com>
2025-01-08 23:48:16 -0800

    Update `BalancedAccuracy***` (#818)

--- src/arkas/analyzer/balanced_accuracy.py
+++ src/arkas/analyzer/balanced_accuracy.py
@@ -57,7 +57,6 @@ class BalancedAccuracyAnalyzer(BaseTruePredAnalyzer):
     >>> output
     BalancedAccuracyOutput(
       (state): AccuracyState(y_true=(6,), y_pred=(6,), y_true_name='target', y_pred_name='pred', nan_policy='propagate')
-      (nan_policy): propagate
     )
 
     ```
@@ -101,6 +100,6 @@ class BalancedAccuracyAnalyzer(BaseTruePredAnalyzer):
                 y_pred=to_array(frame[self._y_pred]).ravel(),
                 y_true_name=self._y_true,
                 y_pred_name=self._y_pred,
+                nan_policy=self._nan_policy,
             ),
-            nan_policy=self._nan_policy,
         )
