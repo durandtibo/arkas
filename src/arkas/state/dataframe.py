1a0640923c06e5fd7722b0b0e4454273104f2f27
Thibaut Durand <durand.tibo@gmail.com>
2025-01-09 18:55:35 -0800

    Improve `DataFrameState` (#820)

--- src/arkas/state/dataframe.py
+++ src/arkas/state/dataframe.py
@@ -4,30 +4,20 @@ from __future__ import annotations
 
 __all__ = ["DataFrameState"]
 
-import sys
 from typing import TYPE_CHECKING, Any
-
-from coola import objects_are_equal
-from coola.utils.format import repr_mapping_line, str_indent, str_mapping
 
 from arkas.figure.utils import get_default_config
 from arkas.metric.utils import check_nan_policy
-from arkas.state.base import BaseState
-
-if sys.version_info >= (3, 11):
-    from typing import Self
-else:  # pragma: no cover
-    from typing_extensions import (
-        Self,  # use backport because it was added in python 3.11
-    )
+from arkas.state.arg import BaseArgState
 
 if TYPE_CHECKING:
+
     import polars as pl
 
     from arkas.figure.base import BaseFigureConfig
 
 
-class DataFrameState(BaseState):
+class DataFrameState(BaseArgState):
     r"""Implement the DataFrame state.
 
     Args:
@@ -36,6 +26,7 @@ class DataFrameState(BaseState):
             arrays. The following options are available: ``'omit'``,
             ``'propagate'``, and ``'raise'``.
         figure_config: An optional figure configuration.
+        **kwargs: Additional keyword arguments.
 
     Example usage:
 
@@ -62,33 +53,13 @@ class DataFrameState(BaseState):
         dataframe: pl.DataFrame,
         nan_policy: str = "propagate",
         figure_config: BaseFigureConfig | None = None,
+        **kwargs: Any,
     ) -> None:
+        super().__init__(**kwargs)
         self._dataframe = dataframe
         check_nan_policy(nan_policy)
         self._nan_policy = nan_policy
         self._figure_config = figure_config or get_default_config()
-
-    def __repr__(self) -> str:
-        args = repr_mapping_line(
-            {
-                "dataframe": self._dataframe.shape,
-                "nan_policy": self._nan_policy,
-                "figure_config": self._figure_config,
-            }
-        )
-        return f"{self.__class__.__qualname__}({args})"
-
-    def __str__(self) -> str:
-        args = str_indent(
-            str_mapping(
-                {
-                    "dataframe": self._dataframe.shape,
-                    "nan_policy": self._nan_policy,
-                    "figure_config": self._figure_config,
-                }
-            )
-        )
-        return f"{self.__class__.__qualname__}({args})"
 
     @property
     def dataframe(self) -> pl.DataFrame:
@@ -102,21 +73,9 @@ class DataFrameState(BaseState):
     def figure_config(self) -> BaseFigureConfig | None:
         return self._figure_config
 
-    def clone(self, deep: bool = True) -> Self:
-        return self.__class__(
-            dataframe=self._dataframe.clone() if deep else self._dataframe,
-            nan_policy=self._nan_policy,
-            figure_config=self._figure_config.clone() if deep else self._figure_config,
-        )
-
-    def equal(self, other: Any, equal_nan: bool = False) -> bool:
-        if not isinstance(other, self.__class__):
-            return False
-        return objects_are_equal(self.get_args(), other.get_args(), equal_nan=equal_nan)
-
     def get_args(self) -> dict:
         return {
             "dataframe": self._dataframe,
             "nan_policy": self._nan_policy,
             "figure_config": self._figure_config,
-        }
+        } | super().get_args()
