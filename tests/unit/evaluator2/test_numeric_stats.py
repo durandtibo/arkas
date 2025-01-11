2d252cd60bec10139b559be78041a064974041de
Thibaut Durand <durand.tibo@gmail.com>
2025-01-10 21:46:56 -0800

    Add `NumericStatisticsEvaluator`

--- tests/unit/evaluator2/test_numeric_stats.py
+++ tests/unit/evaluator2/test_numeric_stats.py
@@ -25,49 +25,122 @@ def dataframe() -> pl.DataFrame:
 ################################################
 
 
-def test_correlation_evaluator_repr(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_repr(dataframe: pl.DataFrame) -> None:
     assert repr(NumericStatisticsEvaluator(DataFrameState(dataframe))).startswith(
         "NumericStatisticsEvaluator("
     )
 
 
-def test_correlation_evaluator_str(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_str(dataframe: pl.DataFrame) -> None:
     assert str(NumericStatisticsEvaluator(DataFrameState(dataframe))).startswith(
         "NumericStatisticsEvaluator("
     )
 
 
-def test_correlation_evaluator_equal_true(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_equal_true(dataframe: pl.DataFrame) -> None:
     assert NumericStatisticsEvaluator(DataFrameState(dataframe)).equal(
         NumericStatisticsEvaluator(DataFrameState(dataframe))
     )
 
 
-def test_correlation_evaluator_equal_false_different_state(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_equal_false_different_state(dataframe: pl.DataFrame) -> None:
     assert not NumericStatisticsEvaluator(DataFrameState(dataframe)).equal(
         NumericStatisticsEvaluator(DataFrameState(pl.DataFrame({"col1": [], "col2": []})))
     )
 
 
-def test_correlation_evaluator_equal_false_different_type(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_equal_false_different_type(dataframe: pl.DataFrame) -> None:
     assert not NumericStatisticsEvaluator(DataFrameState(dataframe)).equal(42)
 
 
-def test_correlation_evaluator_evaluate(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_evaluate(dataframe: pl.DataFrame) -> None:
     evaluator = NumericStatisticsEvaluator(DataFrameState(dataframe))
     assert objects_are_allclose(
         evaluator.evaluate(),
         {
-            "count": 7,
-            "pearson_coeff": -1.0,
-            "pearson_pvalue": 0.0,
-            "spearman_coeff": -1.0,
-            "spearman_pvalue": 0.0,
+            "col1": {
+                "count": 7,
+                "nunique": 7,
+                "num_non_nulls": 7,
+                "num_nulls": 0,
+                "mean": 4.0,
+                "std": 2.0,
+                "skewness": 0.0,
+                "kurtosis": -1.25,
+                "min": 1.0,
+                "q001": 1.006,
+                "q01": 1.06,
+                "q05": 1.3,
+                "q10": 1.6,
+                "q25": 2.5,
+                "median": 4.0,
+                "q75": 5.5,
+                "q90": 6.4,
+                "q95": 6.7,
+                "q99": 6.94,
+                "q999": 6.994,
+                "max": 7.0,
+                ">0": 7,
+                "<0": 0,
+                "=0": 0,
+            },
+            "col2": {
+                "count": 7,
+                "nunique": 7,
+                "num_non_nulls": 7,
+                "num_nulls": 0,
+                "mean": 4.0,
+                "std": 2.0,
+                "skewness": 0.0,
+                "kurtosis": -1.25,
+                "min": 1.0,
+                "q001": 1.006,
+                "q01": 1.06,
+                "q05": 1.3,
+                "q10": 1.6,
+                "q25": 2.5,
+                "median": 4.0,
+                "q75": 5.5,
+                "q90": 6.4,
+                "q95": 6.7,
+                "q99": 6.94,
+                "q999": 6.994,
+                "max": 7.0,
+                ">0": 7,
+                "<0": 0,
+                "=0": 0,
+            },
+            "col3": {
+                "count": 7,
+                "nunique": 3,
+                "num_non_nulls": 7,
+                "num_nulls": 0,
+                "mean": 2.0,
+                "std": 0.7559289460184544,
+                "skewness": 0.0,
+                "kurtosis": -1.25,
+                "min": 1.0,
+                "q001": 1.0,
+                "q01": 1.0,
+                "q05": 1.0,
+                "q10": 1.0,
+                "q25": 1.5,
+                "median": 2.0,
+                "q75": 2.5,
+                "q90": 3.0,
+                "q95": 3.0,
+                "q99": 3.0,
+                "q999": 3.0,
+                "max": 3.0,
+                ">0": 7,
+                "<0": 0,
+                "=0": 0,
+            },
         },
     )
 
 
-def test_correlation_evaluator_evaluate_one_row() -> None:
+def test_numeric_statistics_evaluator_evaluate_one_row() -> None:
     evaluator = NumericStatisticsEvaluator(
         DataFrameState(
             pl.DataFrame(
@@ -78,168 +151,64 @@ def test_correlation_evaluator_evaluate_one_row() -> None:
     assert objects_are_allclose(
         evaluator.evaluate(),
         {
-            "count": 1,
-            "pearson_coeff": float("nan"),
-            "pearson_pvalue": float("nan"),
-            "spearman_coeff": float("nan"),
-            "spearman_pvalue": float("nan"),
+            "col1": {
+                "count": 1,
+                "nunique": 1,
+                "num_non_nulls": 1,
+                "num_nulls": 0,
+                "mean": 1.0,
+                "std": 0.0,
+                "skewness": float("nan"),
+                "kurtosis": float("nan"),
+                "min": 1.0,
+                "q001": 1.0,
+                "q01": 1.0,
+                "q05": 1.0,
+                "q10": 1.0,
+                "q25": 1.0,
+                "median": 1.0,
+                "q75": 1.0,
+                "q90": 1.0,
+                "q95": 1.0,
+                "q99": 1.0,
+                "q999": 1.0,
+                "max": 1.0,
+                ">0": 1,
+                "<0": 0,
+                "=0": 0,
+            },
+            "col2": {
+                "count": 1,
+                "nunique": 1,
+                "num_non_nulls": 1,
+                "num_nulls": 0,
+                "mean": 7.0,
+                "std": 0.0,
+                "skewness": float("nan"),
+                "kurtosis": float("nan"),
+                "min": 7.0,
+                "q001": 7.0,
+                "q01": 7.0,
+                "q05": 7.0,
+                "q10": 7.0,
+                "q25": 7.0,
+                "median": 7.0,
+                "q75": 7.0,
+                "q90": 7.0,
+                "q95": 7.0,
+                "q99": 7.0,
+                "q999": 7.0,
+                "max": 7.0,
+                ">0": 1,
+                "<0": 0,
+                "=0": 0,
+            },
         },
         equal_nan=True,
     )
 
 
-def test_correlation_evaluator_evaluate_nan_policy_omit() -> None:
-    evaluator = NumericStatisticsEvaluator(
-        DataFrameState(
-            pl.DataFrame(
-                {
-                    "col1": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        None,
-                        9.0,
-                        None,
-                        float("nan"),
-                        12.0,
-                        float("nan"),
-                    ],
-                    "col2": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        8.0,
-                        None,
-                        None,
-                        11.0,
-                        float("nan"),
-                        float("nan"),
-                    ],
-                },
-                schema={"col1": pl.Float64, "col2": pl.Float64},
-            ),
-            nan_policy="omit",
-        )
-    )
-    assert objects_are_allclose(
-        evaluator.evaluate(),
-        {
-            "count": 7,
-            "pearson_coeff": 1.0,
-            "pearson_pvalue": 0.0,
-            "spearman_coeff": 1.0,
-            "spearman_pvalue": 0.0,
-        },
-    )
-
-
-def test_correlation_evaluator_evaluate_nan_policy_propagate() -> None:
-    evaluator = NumericStatisticsEvaluator(
-        DataFrameState(
-            pl.DataFrame(
-                {
-                    "col1": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        None,
-                        9.0,
-                        None,
-                        float("nan"),
-                        12.0,
-                        float("nan"),
-                    ],
-                    "col2": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        8.0,
-                        None,
-                        None,
-                        11.0,
-                        float("nan"),
-                        float("nan"),
-                    ],
-                },
-                schema={"col1": pl.Float64, "col2": pl.Float64},
-            ),
-        )
-    )
-    assert objects_are_allclose(
-        evaluator.evaluate(),
-        {
-            "count": 13,
-            "pearson_coeff": float("nan"),
-            "pearson_pvalue": float("nan"),
-            "spearman_coeff": float("nan"),
-            "spearman_pvalue": float("nan"),
-        },
-        equal_nan=True,
-    )
-
-
-def test_correlation_evaluator_evaluate_nan_policy_raise() -> None:
-    evaluator = NumericStatisticsEvaluator(
-        DataFrameState(
-            pl.DataFrame(
-                {
-                    "col1": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        None,
-                        9.0,
-                        None,
-                        float("nan"),
-                        12.0,
-                        float("nan"),
-                    ],
-                    "col2": [
-                        1.0,
-                        2.0,
-                        3.0,
-                        4.0,
-                        5.0,
-                        6.0,
-                        7.0,
-                        8.0,
-                        None,
-                        None,
-                        11.0,
-                        float("nan"),
-                        float("nan"),
-                    ],
-                },
-                schema={"col1": pl.Float64, "col2": pl.Float64},
-            ),
-            nan_policy="raise",
-        )
-    )
-    with pytest.raises(ValueError, match="'x' contains at least one NaN value"):
-        evaluator.evaluate()
-
-
-def test_correlation_evaluator_evaluate_empty() -> None:
+def test_numeric_statistics_evaluator_evaluate_empty() -> None:
     evaluator = NumericStatisticsEvaluator(DataFrameState(pl.DataFrame({"col1": [], "col2": []})))
     assert objects_are_allclose(
         evaluator.evaluate(),
@@ -301,7 +270,7 @@ def test_correlation_evaluator_evaluate_empty() -> None:
     )
 
 
-def test_correlation_evaluator_evaluate_prefix_suffix(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_evaluate_prefix_suffix(dataframe: pl.DataFrame) -> None:
     evaluator = NumericStatisticsEvaluator(DataFrameState(dataframe))
     assert objects_are_allclose(
         evaluator.evaluate(prefix="prefix_", suffix="_suffix"),
@@ -388,18 +357,91 @@ def test_correlation_evaluator_evaluate_prefix_suffix(dataframe: pl.DataFrame) -
     )
 
 
-def test_correlation_evaluator_compute(dataframe: pl.DataFrame) -> None:
+def test_numeric_statistics_evaluator_compute(dataframe: pl.DataFrame) -> None:
     assert (
         NumericStatisticsEvaluator(DataFrameState(dataframe))
         .compute()
-        .equal(
+        .allclose(
             Evaluator(
                 {
-                    "count": 7,
-                    "pearson_coeff": -1.0,
-                    "pearson_pvalue": 0.0,
-                    "spearman_coeff": -1.0,
-                    "spearman_pvalue": 0.0,
+                    "col1": {
+                        "count": 7,
+                        "nunique": 7,
+                        "num_non_nulls": 7,
+                        "num_nulls": 0,
+                        "mean": 4.0,
+                        "std": 2.0,
+                        "skewness": 0.0,
+                        "kurtosis": -1.25,
+                        "min": 1.0,
+                        "q001": 1.006,
+                        "q01": 1.06,
+                        "q05": 1.3,
+                        "q10": 1.6,
+                        "q25": 2.5,
+                        "median": 4.0,
+                        "q75": 5.5,
+                        "q90": 6.4,
+                        "q95": 6.7,
+                        "q99": 6.94,
+                        "q999": 6.994,
+                        "max": 7.0,
+                        ">0": 7,
+                        "<0": 0,
+                        "=0": 0,
+                    },
+                    "col2": {
+                        "count": 7,
+                        "nunique": 7,
+                        "num_non_nulls": 7,
+                        "num_nulls": 0,
+                        "mean": 4.0,
+                        "std": 2.0,
+                        "skewness": 0.0,
+                        "kurtosis": -1.25,
+                        "min": 1.0,
+                        "q001": 1.006,
+                        "q01": 1.06,
+                        "q05": 1.3,
+                        "q10": 1.6,
+                        "q25": 2.5,
+                        "median": 4.0,
+                        "q75": 5.5,
+                        "q90": 6.4,
+                        "q95": 6.7,
+                        "q99": 6.94,
+                        "q999": 6.994,
+                        "max": 7.0,
+                        ">0": 7,
+                        "<0": 0,
+                        "=0": 0,
+                    },
+                    "col3": {
+                        "count": 7,
+                        "nunique": 3,
+                        "num_non_nulls": 7,
+                        "num_nulls": 0,
+                        "mean": 2.0,
+                        "std": 0.7559289460184544,
+                        "skewness": 0.0,
+                        "kurtosis": -1.25,
+                        "min": 1.0,
+                        "q001": 1.0,
+                        "q01": 1.0,
+                        "q05": 1.0,
+                        "q10": 1.0,
+                        "q25": 1.5,
+                        "median": 2.0,
+                        "q75": 2.5,
+                        "q90": 3.0,
+                        "q95": 3.0,
+                        "q99": 3.0,
+                        "q999": 3.0,
+                        "max": 3.0,
+                        ">0": 7,
+                        "<0": 0,
+                        "=0": 0,
+                    },
                 },
             )
         )
