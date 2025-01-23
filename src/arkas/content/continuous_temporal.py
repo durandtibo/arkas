c7ca4d8141ccf4b08e340a3dd5d9fea790efc8e1
Thibaut Durand <durand.tibo@gmail.com>
2025-01-22 23:13:58 -0800

    Update `TemporalContinuousColumnContentGenerator`

--- src/arkas/content/continuous_temporal.py
+++ src/arkas/content/continuous_temporal.py
@@ -12,10 +12,9 @@ from coola.utils import repr_indent, repr_mapping, str_indent, str_mapping
 from jinja2 import Template
 
 from arkas.content.section import BaseSectionContentGenerator
-from arkas.utils.dataframe import check_num_columns
 
 if TYPE_CHECKING:
-    from arkas.state.temporal_dataframe import TemporalDataFrameState
+    from arkas.state.temporal_column import TemporalColumnState
 
 
 logger = logging.getLogger(__name__)
@@ -35,10 +34,11 @@ class TemporalContinuousColumnContentGenerator(BaseSectionContentGenerator):
     >>> from datetime import datetime, timezone
     >>> import polars as pl
     >>> from arkas.content import TemporalContinuousColumnContentGenerator
-    >>> from arkas.state import TemporalDataFrameState
+    >>> from arkas.state import TemporalColumnState
     >>> frame = pl.DataFrame(
     ...     {
-    ...         "col1": [0, 1, 2, 3],
+    ...         "col1": [0, 1, 0, 1],
+    ...         "col2": [0, 1, 2, 3],
     ...         "datetime": [
     ...             datetime(year=2020, month=1, day=3, tzinfo=timezone.utc),
     ...             datetime(year=2020, month=2, day=3, tzinfo=timezone.utc),
@@ -48,22 +48,22 @@ class TemporalContinuousColumnContentGenerator(BaseSectionContentGenerator):
     ...     },
     ...     schema={
     ...         "col1": pl.Int64,
+    ...         "col2": pl.Int64,
     ...         "datetime": pl.Datetime(time_unit="us", time_zone="UTC"),
     ...     },
     ... )
     >>> content = TemporalContinuousColumnContentGenerator(
-    ...     TemporalDataFrameState(frame, temporal_column="datetime")
+    ...     TemporalColumnState(frame, target_column="col2", temporal_column="datetime")
     ... )
     >>> content
     TemporalContinuousColumnContentGenerator(
-      (state): TemporalDataFrameState(dataframe=(4, 2), temporal_column='datetime', period=None, nan_policy='propagate', figure_config=MatplotlibFigureConfig())
+      (state): TemporalColumnState(dataframe=(4, 3), target_column='col2', temporal_column='datetime', period=None, nan_policy='propagate', figure_config=MatplotlibFigureConfig())
     )
 
     ```
     """
 
-    def __init__(self, state: TemporalDataFrameState) -> None:
-        check_num_columns(state.dataframe, num_columns=2)
+    def __init__(self, state: TemporalColumnState) -> None:
         self._state = state
 
     def __repr__(self) -> str:
@@ -80,14 +80,13 @@ class TemporalContinuousColumnContentGenerator(BaseSectionContentGenerator):
         return self._state.equal(other._state, equal_nan=equal_nan)
 
     def generate_content(self) -> str:
-        first, second = self._state.dataframe.columns
-        col = first
-        if first == self._state.temporal_column:
-            col = second
-        logger.info(f"Generating the temporal continuous distribution analysis of {col!r}...")
+        logger.info(
+            "Generating the temporal continuous distribution analysis of "
+            f"{self._state.target_column!r}..."
+        )
         return Template(create_template()).render(
             {
-                "column": col,
+                "column": self._state.target_column,
                 "temporal_column": self._state.temporal_column,
             }
         )
