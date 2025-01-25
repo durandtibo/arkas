b439eb4990f017018d8589acd8dc65b701c43afd
Thibaut Durand <durand.tibo@gmail.com>
2025-01-24 23:25:36 -0800

    Add `BaseCacheEvaluator`

---
+++ src/arkas/evaluator2/caching.py
@@ -0,0 +1,31 @@
+r"""Define the base class to implement an evaluator that cache the
+metrics after the first evaluation."""
+
+from __future__ import annotations
+
+__all__ = ["BaseCacheEvaluator"]
+
+from abc import abstractmethod
+
+from arkas.evaluator2.base import BaseEvaluator
+
+
+class BaseCacheEvaluator(BaseEvaluator):
+    r"""Define the base class to implement an evaluator that cache the
+    metrics after the first evaluation."""
+
+    def __init__(self) -> None:
+        self._cached_data = None
+
+    def evaluate(self, prefix: str = "", suffix: str = "") -> dict:
+        if self._cached_data is None:
+            self._cached_data = self._evaluate()
+        return {f"{prefix}{col}{suffix}": val for col, val in self._cached_data.items()}
+
+    @abstractmethod
+    def _evaluate(self) -> dict:
+        r"""Evaluate the metrics.
+
+        Returns:
+            The metrics.
+        """
